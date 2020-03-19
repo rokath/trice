39f144bdf6ac8ac58085b75134367345791a46da
Thomas Höhenleitner <th@seerose.net>
2020-03-19 07:59:25 +0100

    client server functionality started

--- cmd/trice/handleArgs.go
+++ cmd/trice/handleArgs.go
@@ -11,6 +11,8 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net"
+	"net/rpc"
 	"os"
 	"path/filepath"
 
@@ -33,11 +35,17 @@ func (i *arrayFlag) Set(value string) error {
 
 var srcs arrayFlag // gets multiple files or directories
 
+var port string
+var baud int
+var fnJSON string
+var pList *id.List
+var ipAddr string
+var ipPort string
 
 // HandleArgs evaluates the arguments slice of strings und uses wd as working directory
 func HandleArgs(wd string, args []string) error {
 	list := make(id.List, 0, 65536) // for 16 bit IDs enough
-	pList := &list
+	pList = &list
 
 	uCmd := flag.NewFlagSet("update", flag.ExitOnError)                                        // subcommand
 	pDryR := uCmd.Bool("dry-run", false, "no changes are applied (optional)")                  // flag
@@ -47,7 +55,7 @@ func HandleArgs(wd string, args []string) error {
 
 	lCmd := flag.NewFlagSet("log", flag.ExitOnError)                                // subcommand
 	pPort := lCmd.String("port", "", "subcommand (required, try COMscan)")          // flag
-	pBaud := lCmd.Int("baud", 38400, "COM baudrate (optional, default is 38400")    // flag
+	pBaud := lCmd.Int("baud", 115200, "COM baudrate (optional, default is 115200")  // flag
 	pJSON := lCmd.String("list", "til.json", "trice ID list path (optional)")       // flag
 	pCol := lCmd.String("color", "default", "color set (optional), off, alternate") // flag
 	pKey := lCmd.String("key", "none", "decrypt passphrase, (optional)")            // flag
@@ -66,6 +74,21 @@ func HandleArgs(wd string, args []string) error {
 	hCmd := flag.NewFlagSet("help", flag.ContinueOnError) // subcommand
 
 	vCmd := flag.NewFlagSet("version", flag.ContinueOnError) // subcommand
+
+	svCmd := flag.NewFlagSet("displayServer", flag.ExitOnError)                               // subcommand
+	pSvIPA := svCmd.String("ipa", "localhost", "ip address (optional, default is localhost)") // flag (127.0.0.1)
+	pSvIPP := svCmd.String("ipp", "", "port number (required, a 16 bit number)")              // flag
+	pSvCol := svCmd.String("color", "default", "color set (optional), off, alternate")        // flag
+	pSvTs := svCmd.String("ts", "LOCmicro", "timestamp (optional), off, UTCmicro")            // flag
+
+	clCmd := flag.NewFlagSet("remoteDisplay", flag.ExitOnError)                               // subcommand
+	pClIPA := clCmd.String("ipa", "localhost", "ip address (optional, default is localhost)") // flag (127.0.0.1)
+	pClIPP := clCmd.String("ipp", "", "ip port number (required, a 16 bit number)")           // flag
+	pClCol := clCmd.String("color", "default", "color set (optional), off, alternate")        // flag
+	pClTs := clCmd.String("ts", "LOCmicro", "timestamp (optional), off, UTCmicro")            // flag
+	pClPort := clCmd.String("port", "", "subcommand (required, try COMscan)")                 // flag
+	pClBaud := clCmd.Int("baud", 115200, "COM baudrate (optional, default is 115200")         // flag
+	pClJSON := clCmd.String("list", "til.json", "trice ID list path (optional)")              // flag
 
 	// Verify that a subcommand has been provided
 	// os.Arg[0] is the main command
@@ -98,6 +121,10 @@ func HandleArgs(wd string, args []string) error {
 		err = lCmd.Parse(subArgs)
 	case "zeroSourceTreeIds":
 		err = zCmd.Parse(subArgs)
+	case "displayServer":
+		err = svCmd.Parse(subArgs)
+	case "remoteDisplay":
+		err = clCmd.Parse(subArgs)
 	default:
 		fmt.Println("try: 'trice help|h'")
 		return nil
@@ -108,7 +135,7 @@ func HandleArgs(wd string, args []string) error {
 	// Check which subcommand was Parsed using the FlagSet.Parsed() function. Handle each case accordingly.
 	// FlagSet.Parse() will evaluate to false if no flags were parsed (i.e. the user did not provide any flags)
 	if hCmd.Parsed() {
-		return help(hCmd, uCmd, cCmd, lCmd, zCmd, vCmd)
+		return help(hCmd, uCmd, cCmd, lCmd, zCmd, vCmd, svCmd, clCmd)
 	}
 	if uCmd.Parsed() {
 		lU, err := filepath.Abs(*pLU)
@@ -123,7 +150,7 @@ func HandleArgs(wd string, args []string) error {
 				return fmt.Errorf("failed to parse %s: %v", srcU, err)
 			}
 			if _, err := os.Stat(srcU); err == nil { // path exists
-				err = update(*pDryR, srcU, lU, pList, *pVerb)
+				err = update(*pDryR, srcU, lU /*pList, */, *pVerb)
 				if nil != err {
 					return err
 				}
@@ -138,12 +165,15 @@ func HandleArgs(wd string, args []string) error {
 		return nil
 	}
 	if cCmd.Parsed() {
-		return checkList(*pC, *pSet, pList, *pPal)
+		return checkList(*pC, *pSet /*pList,*/, *pPal)
 	}
 	if lCmd.Parsed() {
 		emit.TimeStampFormat = *pTs
 		emit.ColorPalette = *pCol
-		return logTrices(lCmd, *pPort, *pBaud, *pJSON, pList, *pKey, *pShow)
+		baud = *pBaud
+		port = *pPort
+		fnJSON = *pJSON
+		return logTrices( /*lCmd, pList,*/ *pKey, *pShow)
 	}
 	if zCmd.Parsed() {
 		return zeroIds(*pRunZ, *pSrcZ, zCmd)
@@ -151,6 +181,24 @@ func HandleArgs(wd string, args []string) error {
 	if vCmd.Parsed() {
 		return ver()
 	}
+	if svCmd.Parsed() {
+		emit.TimeStampFormat = *pSvTs
+		emit.ColorPalette = *pSvCol
+		ipAddr = *pSvIPA
+		ipPort = *pSvIPP
+		return displayServer()
+	}
+	if clCmd.Parsed() {
+		emit.TimeStampFormat = *pClTs
+		emit.ColorPalette = *pClCol
+		port = *pClPort
+		baud = *pClBaud
+		ipAddr = *pClIPA
+		ipPort = *pClIPP
+		fnJSON = *pClJSON
+		return remoteDisplay()
+	}
+
 	return nil
 }
 
@@ -164,24 +212,30 @@ func ver() error {
 }
 
 func help(hCmd *flag.FlagSet,
-	uCmd *flag.FlagSet,
-	cCmd *flag.FlagSet,
-	lCmd *flag.FlagSet,
-	zCmd *flag.FlagSet,
-	vCmd *flag.FlagSet) error {
+	u *flag.FlagSet,
+	c *flag.FlagSet,
+	l *flag.FlagSet,
+	z *flag.FlagSet,
+	v *flag.FlagSet,
+	sv *flag.FlagSet,
+	cl *flag.FlagSet) error {
 	fmt.Println("syntax: 'trice subcommand' [params]")
 	fmt.Println("subcommand 'help', 'h'")
 	hCmd.PrintDefaults()
 	fmt.Println("subcommand 'update', 'upd', 'u'")
-	uCmd.PrintDefaults()
+	u.PrintDefaults()
 	fmt.Println("subcommand 'check'")
-	cCmd.PrintDefaults()
+	c.PrintDefaults()
 	fmt.Println("subcommand 'log', 'l'")
-	lCmd.PrintDefaults()
+	l.PrintDefaults()
 	fmt.Println("subcommand 'zeroSourceTreeIds' (avoid using this subcommand normally)")
-	zCmd.PrintDefaults()
+	z.PrintDefaults()
 	fmt.Println("subcommand 'version', 'ver'. 'v'")
-	vCmd.PrintDefaults()
+	v.PrintDefaults()
+	fmt.Println("subcommand 'displayServer'")
+	sv.PrintDefaults()
+	fmt.Println("subcommand 'remoteDisplay'")
+	cl.PrintDefaults()
 	fmt.Println("examples:")
 	fmt.Println("    'trice update [-src sourcerootdir]', default sourcerootdir is ./")
 	fmt.Println("    'trice log [-port COMn] [-baud m]', default port is COMscan, default m is 38400, fixed to 8N1")
@@ -191,23 +245,23 @@ func help(hCmd *flag.FlagSet,
 }
 
 // parse source tree, update IDs and is list
-func update(dryRun bool, dir, fn string, p *id.List, verbose bool) error {
-	err := p.Update(dir, fn, !dryRun, verbose)
+func update(dryRun bool, dir, fn string /*p *id.List,*/, verbose bool) error {
+	err := pList.Update(dir, fn, !dryRun, verbose)
 	if nil != err {
 		return fmt.Errorf("failed update on %s with %s: %v", dir, fn, err)
 	}
-	fmt.Println(len(*p), "ID's in list", fn)
+	fmt.Println(len(*pList), "ID's in list", fn)
 	return nil
 }
 
 // log the id list with dataset
-func checkList(fn, dataset string, p *id.List, palette string) error {
-	err := p.Read(fn)
+func checkList(fn, dataset string /*p *id.List,*/, palette string) error {
+	err := pList.Read(fn)
 	if nil != err {
 		fmt.Println("ID list " + fn + " not found, exit")
 		return nil
 	}
-	emit.Check(*p, dataset)
+	emit.Check(*pList, dataset)
 	return nil
 }
 
@@ -235,17 +289,17 @@ func createCipher(password string, show bool) (*xtea.Cipher, bool, error) {
 }
 
 // connect to port and display traces
-func logTrices(cmd *flag.FlagSet, port string, baud int, fn string, p *id.List, password string, show bool) error {
-	if "" == port {
+func logTrices( /*cmd *flag.FlagSet, fn string  p *id.List, */ password string, show bool) error {
+	/*if "" == port {
 		cmd.PrintDefaults()
 		return nil
-	}
+	}*/
 
-	if "none" != fn {
+	if "none" != fnJSON {
 		// setup ip list
-		err := p.Read(fn)
+		err := pList.Read(fnJSON)
 		if nil != err {
-			fmt.Println("ID list " + fn + " not found, exit")
+			fmt.Println("ID list " + fnJSON + " not found, exit")
 			return nil
 		}
 	}
@@ -262,28 +316,32 @@ func logTrices(cmd *flag.FlagSet, port string, baud int, fn string, p *id.List, 
 	   3) HTTP/Websocket receiver (may be the simplest form in Golang)
 	*/
 
-	if port == "COMscan" {
-		log.Println("Scan for serial ports...")
-
-		ports, err := receiver.GetSerialPorts()
-
-		if err != nil {
-			log.Fatal("Error during serial port search", err)
-		}
-
-		if len(ports) > 0 {
-			log.Println("Take serial port", ports[0])
-			port = ports[0]
-		} else {
-			log.Fatal("Could not find serial port on system")
-			return nil
-		}
-	}
-	fmt.Println("id list file", fn, "with", len(*p), "items")
-	return doSerialReceive(port, baud, p)
+	fmt.Println("id list file", fnJSON, "with", len(*pList), "items")
+	return doSerialReceive()
 }
 
-func doSerialReceive(port string, baud int, p *id.List) error {
+func comScan() error {
+	log.Println("Scan for serial ports...")
+	ports, err := receiver.GetSerialPorts()
+	if err != nil {
+		return err
+	}
+	if len(ports) > 0 {
+		log.Println("Take serial port", ports[0])
+		port = ports[0]
+		return nil
+	} else {
+		return errors.New("Could not find serial port on system")
+	}
+}
+
+func doSerialReceive() error {
+	if port == "COMscan" {
+		err := comScan()
+		if err != nil {
+			return err
+		}
+	}
 	serialReceiver := receiver.NewSerialReceiver(port, baud)
 
 	if serialReceiver.SetUp() == false {
@@ -307,7 +365,7 @@ func doSerialReceive(port string, baud int, p *id.List) error {
 			}
 		case t = <-(*serialReceiver.GetTriceChannel()):
 			//log.Println("from trice channel:", t)
-			b, err = emit.Trice(t, b, *p)
+			b, err = emit.Trice(t, b, *pList)
 			if nil != err {
 				log.Println("trice.Log error", err, t, b)
 			}
@@ -322,5 +380,46 @@ func zeroIds(dryRun bool, SrcZ string, cmd *flag.FlagSet) error {
 		return errors.New("no source tree root specified")
 	}
 	id.ZeroSourceTreeIds(SrcZ, !dryRun)
+	return nil
+}
+
+// Server may be can be unexported
+type Server struct{}
+
+// Display is the exported server function for string display
+func (p *Server) Display(s string, reply *int64) error {
+	*reply = 7
+	return emit.Visualize(s)
+}
+
+/* Adder is a demo
+func (p *Server) Adder(u [2]int64, reply *int64) error {
+	*reply = u[0] + u[1]
+	return nil
+}*/
+
+func displayServer() error {
+	a := fmt.Sprintf("%s:%s", ipAddr, ipPort)
+	fmt.Println("displayServer @", a)
+	rpc.Register(new(Server))
+
+	ln, err := net.Listen("tcp", a)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	for {
+		c, err := ln.Accept()
+		if err != nil {
+			continue
+		}
+		go rpc.ServeConn(c)
+	}
+}
+
+func remoteDisplay() error {
+	a := fmt.Sprintf("%s:%s", ipAddr, ipPort)
+	fmt.Println("remoteDisplay @", a, "port=", port, "baud=", baud)
+	// todo
 	return nil
 }
