2d1edd07a93118e1d59de6a89d10371b3eda2628
Thomas Höhenleitner <th@seerose.net>
2020-06-18 22:35:57 +0200

    all tests ok

--- pkg/inputDummy/inputDummy.go
+++ pkg/inputDummy/inputDummy.go
@@ -8,22 +8,24 @@
 // The time.Duration value can be used to delay the Read function.
 package inputDummy
 
-import "time"
+import (
+	"io"
+	"time"
+)
 
 // Source delivers an endless circle of bytes.
 //
 //
 type Source struct {
-	b []byte        // internal byte buffer to read from
-	t time.Duration // tinternal delay
+	b      []byte        // internal byte buffer to read from
+	t      time.Duration // tinternal delay
+	offset int           // next read position
 }
 
 // New creates an instance of simulator.
 // b contains bytes it reads cyclically
 func New(b []byte, t time.Duration) *Source {
-	s := &Source{
-		//t: []byte{'g', 'a', 'r', 'b', 'a', 'g', 'e', 235, 96, 96, 0, 10, 172, 0, 0},
-	}
+	s := &Source{}
 	s.b = b
 	s.t = t
 	return s
@@ -33,10 +35,16 @@ func New(b []byte, t time.Duration) *Source {
 func (p *Source) Read(b []byte) (int, error) {
 	var i int
 	time.Sleep(p.t)
-	//p.b[6]++                                                              // increment par
-	//p.b[3] = p.b[0] ^ p.b[1] ^ p.b[2] ^ p.b[4] ^ p.b[5] ^ p.b[6] ^ p.b[7] // compute crc
-	for i = range b {
-		b[i] = p.b[i%len(p.b)]
+	if nil == p.b || 0 == len(p.b) {
+		return 0, io.EOF
 	}
-	return i + 1, nil
+	if 0 == len(b) {
+		return 0, nil
+	}
+	for i = range b {
+		b[i] = p.b[(p.offset+i)%len(p.b)]
+	}
+	i++ // adjust to byte count
+	p.offset += i
+	return i, nil
 }
--- pkg/inputDummy/inputDummy_test.go
+++ pkg/inputDummy/inputDummy_test.go
@@ -5,49 +5,87 @@
 package inputDummy_test
 
 import (
+	"io"
 	"testing"
-	"time"
 
-	"github.com/rokath/trice/internal/receiver/inputDummy"
+	"github.com/rokath/trice/pkg/inputDummy"
 	"github.com/rokath/trice/pkg/lib"
 )
 
-// Source delivers an endless circle of bytes.
-//
-//
-type Source struct {
-	b []byte        // internal byte buffer to read from
-	t time.Duration // tinternal delay
-}
-
-// New creates an instance of simulator.
-// b contains bytes it reads cyclically
-func New(b []byte, t time.Duration) *Source {
-	s := &Source{
-		//t: []byte{'g', 'a', 'r', 'b', 'a', 'g', 'e', 235, 96, 96, 0, 10, 172, 0, 0},
-	}
-	s.b = b
-	s.t = t
-	return s
-}
-
-// Read is the exported method.
-func (p *Source) Read(b []byte) (int, error) {
-	var i int
-	time.Sleep(p.t)
-	//p.b[6]++                                                              // increment par
-	//p.b[3] = p.b[0] ^ p.b[1] ^ p.b[2] ^ p.b[4] ^ p.b[5] ^ p.b[6] ^ p.b[7] // compute crc
-	for i = range b {
-		b[i] = p.b[i%len(p.b)]
-	}
-	return i + 1, nil
-}
-
 func Test1(t *testing.T) {
 	i := []byte{'g', 'a', 'r', 'b', 'a', 'g', 'e', 235, 96, 96, 0, 10, 172, 0, 0}
-	s := inputDummy.New(b, 0)
+	s := inputDummy.New(i, 0)
 	o := make([]byte, 7)
 	r := []byte{'g', 'a', 'r', 'b', 'a', 'g', 'e'}
 	s.Read(o)
-	lib.Equals(t, i, o)
+	lib.Equals(t, o, r)
+}
+
+// Test2 checks if a shorter internal buffer repeats
+func Test2(t *testing.T) {
+	i := []byte{'A'}
+	s := inputDummy.New(i, 0)
+	o := make([]byte, 3)
+	r := []byte{'A', 'A', 'A'}
+	s.Read(o)
+	lib.Equals(t, o, r)
+}
+
+// Test 3 checks if a longer internal buffer repeats
+func Test3(t *testing.T) {
+	i := []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	s := inputDummy.New(i, 0)
+	o := make([]byte, 7)
+	exp := []byte{7, 8, 9, 10, 0, 1, 2}
+	s.Read(o)
+	s.Read(o)
+	lib.Equals(t, exp, o)
+}
+
+// Test4 checks if a zero internal buffer works
+func Test4(t *testing.T) {
+	i := []byte{}
+	s := inputDummy.New(i, 0)
+	o := make([]byte, 3)
+	n, err := s.Read(o)
+	lib.Equals(t, n, 0)
+	lib.Equals(t, err, io.EOF)
+}
+
+// Test5 checks if a nil internal buffer works
+func Test5(t *testing.T) {
+	s := inputDummy.New(nil, 0)
+	o := make([]byte, 3)
+	n, err := s.Read(o)
+	lib.Equals(t, n, 0)
+	lib.Equals(t, err, io.EOF)
+}
+
+// Test6 checks if a zero external buffer works with internal nil
+func Test6(t *testing.T) {
+	s := inputDummy.New(nil, 0)
+	o := []byte{}
+	n, err := s.Read(o)
+	lib.Equals(t, n, 0)
+	lib.Equals(t, err, io.EOF)
+}
+
+// Test7 checks if a zero external buffer works with internal zero
+func Test7(t *testing.T) {
+	var i []byte
+	s := inputDummy.New(i, 0)
+	o := []byte{}
+	n, err := s.Read(o)
+	lib.Equals(t, n, 0)
+	lib.Equals(t, err, io.EOF)
+}
+
+// Test8 checks if a zero external buffer works with internal non-zero
+func Test8(t *testing.T) {
+	i := []byte{'g', 'a', 'r', 'b', 'a', 'g', 'e'}
+	s := inputDummy.New(i, 0)
+	var o []byte
+	n, err := s.Read(o)
+	lib.Equals(t, n, 0)
+	lib.Equals(t, err, nil)
 }
