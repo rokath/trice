a9746a3ed135a22ebbff32e361f77a27cf8fce7f
rokath <th@seerose.net>
2020-09-18 14:56:19 +0200

    MDK-ARM_LL_UART_BARE_NUCLEO-F070RB fresh generated

--- test/MDK-ARM_LL_UART_ESC_NUCLEO-F070RB/Core/Src/main.c
+++ test/MDK-ARM_LL_UART_ESC_NUCLEO-F070RB/Core/Src/main.c
@@ -96,10 +96,10 @@ int main(void)
   MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
     LL_USART_EnableIT_RXNE(TRICE_UART); // enable UART2 interrupt
-    TRICE0( Id(54442), "s:                                        \ns:   ARM-MDK_LL_UART_BARE_NUCLEO-F070RB   \ns:                                        \n\n");
+    TRICE0( Id(0), "s:                                        \ns:   ARM-MDK_LL_UART_ESC_NUCLEO-F070RB   \ns:                                        \n\n");
 /* USER CODE END 2 */
 
-  /* Infinite loop */	
+  /* Infinite loop */
   /* USER CODE BEGIN WHILE */
     while (1)
     {
