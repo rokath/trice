98a9b08aeaf5bc06b84c5dbea06d350afca770f4
rokath <th@seerose.net>
2021-09-19 22:22:00 +0200

    MDK-ARM_LL_UART_RTT0_COBSR_STM32F030R8-NUCLEO-64 compiles

--- pkg/src/triceCOBSR_test.c
+++ pkg/src/triceCOBSR_test.c
@@ -1,8 +1,7 @@
 
 #include "trice.h"
+#include "intern/cobsr.h"
 
-#if 0
-#include "cobsr.h"
 //! COBSREncodeWithShortRules does the same as the COBSREncode function but a bit faster for len 1,2,3,4 when first 2 bytes both !=0.
 //! \param dst is the result buffer. It must be at least 1 byte longer than len.
 //! \param src is the source buffer.
@@ -535,7 +534,6 @@ void COBSREncodingCheck128( void ){
     len = (0 < len && len < 128) ? len + 1 : 1;
 }
 
-#endif
 
 //  //! Wrap_cobsr_encode calls the original cobsr_encode function but has a simpler sinature.
 //  //! It is usable for verifying.
@@ -561,3 +559,105 @@ void triceRpcList( void ){
      TriceRpc0( id(0), "cmd:LED %d", SetLed); // experimental send command
 }
 */
+
+
+volatile uint8_t led = 0;
+
+void SetLED( uint8_t state ){
+    led = state;
+}
+
+#define TriceRpc0( id, pfmt, n )
+
+void triceRpcList( void ){
+     TriceRpc0( id(0), "cmd:LED %d", SetLed); // experimental send command
+}
+
+#include "intern/cobsr.h"
+
+void COBSRCheck( void* pTestValue, int byteWidth ){
+    cobsr_encode_result COBSEncResult = {0};
+    cobsr_decode_result COBSDecResult = {0};
+    uint8_t COBSEncBuf[12], COBSDecBuf[12];
+    //static int lastMs = 0;
+    //extern int milliSecond;
+    ////while( milliSecond < lastMs + 10 );
+    //lastMs = milliSecond;
+    switch( byteWidth ){
+        case 1: TRICE16( Id( 47229), "msg:%02x -> ", *(uint8_t*)pTestValue ); break;
+        case 2: TRICE32( Id( 59542), "msg:%04x -> ", *(uint16_t*)pTestValue ); break;
+        default: return;
+    }
+    COBSEncResult = cobsr_encode( 
+    COBSEncBuf, //void * dst_buf_ptr, 
+    sizeof( COBSEncBuf ), // size_t dst_buf_len,
+    pTestValue, // const void * src_ptr, 
+    byteWidth ); // size_t src_len)
+    if( COBSR_ENCODE_OK != COBSEncResult.status ){
+        TRICE16( Id( 55427), "err: i = %d, COBSEncResult.status = %d\n", 0, COBSEncResult.status );
+    }else{
+        switch( COBSEncResult.out_len ){
+            case 1: TRICE16( Id( 37961), "sig:%02x\n", COBSEncBuf[0]); break;
+            case 2: TRICE16( Id( 41310), "sig:%02x %02x\n", COBSEncBuf[0], COBSEncBuf[1]); break;
+            case 3: TRICE16( Id( 32787), "sig:%02x %02x %02x\n", COBSEncBuf[0], COBSEncBuf[1], COBSEncBuf[2]); break;
+            case 4: TRICE16( Id( 37461), "sig:%02x %02x %02x %02x\n", COBSEncBuf[0], COBSEncBuf[1], COBSEncBuf[2], COBSEncBuf[3]); break;
+        }
+    }
+    
+    COBSDecResult = cobsr_decode(
+    COBSDecBuf, // void * dst_buf_ptr, 
+    sizeof( COBSDecBuf ), // size_t dst_buf_len,
+    COBSEncBuf, // const void * src_ptr,
+    COBSEncResult.out_len); // size_t src_len);
+    
+    if( COBSR_DECODE_OK != COBSDecResult.status ){
+        TRICE16( Id( 35623), "err: i = %d, COBSDecResult.status = %d\n", 0, COBSDecResult.status );
+    }else{
+        if( (byteWidth != COBSDecResult.out_len)  ){
+            TRICE16( Id( 53032), "err: COBSDecResult.out_len = %d != byteWidth = %x\n", COBSDecResult.out_len, byteWidth);
+            return;
+        }
+        switch( byteWidth ) {
+            case 1: if( *(uint8_t*)pTestValue != COBSDecBuf[0] ){
+                // err
+            }
+            return;
+            case 2: if( *(uint16_t*)pTestValue != *(uint16_t*)COBSDecBuf ){
+                // err
+            }
+            return;
+        }
+    }
+}
+
+static uint8_t oneByteArray[] = {
+    0,      1,      2,      3,   0xFC,   0xFD,   0xFE,   0xFF 
+};
+
+static uint16_t twoByteArray[] = { 
+    0x0000, 0x0001, 0x0002, 0x0003, 0x00fc, 0x00fd, 0x00fe, 0x00ff,
+    0x0100, 0x0101, 0x0102, 0x0103, 0x01fc, 0x01fd, 0x01fe, 0x01ff,
+    0x0200, 0x0201, 0x0202, 0x0203, 0x02fc, 0x02fd, 0x02fe, 0x02ff,
+    0x0300, 0x0301, 0x0302, 0x0303, 0x03fc, 0x03fd, 0x03fe, 0x03ff,
+    0xfc00, 0xfc01, 0xfc02, 0xfc03, 0xfcfc, 0xfcfd, 0xfcfe, 0xfcff,
+    0xfd00, 0xfd01, 0xfd02, 0xfd03, 0xfdfc, 0xfdfd, 0xfdfe, 0xfdff,
+    0xfe00, 0xfe01, 0xfe02, 0xfe03, 0xfefc, 0xfefd, 0xfefe, 0xfeff,
+    0xff00, 0xff01, 0xff02, 0xff03, 0xfffc, 0xfffd, 0xfffe, 0xffff 
+};
+
+static void COBSRLoopCheck( void ){
+    for( int i = 0; i < sizeof( oneByteArray); i++ ){
+        COBSRCheck( &oneByteArray[i], 1 );
+    }
+    for( int i = 0; i < sizeof( twoByteArray) / sizeof( uint16_t ); i++ ){
+        COBSRCheck( &twoByteArray[i], 2 );
+    }
+}
+       
+void testtest( void ){
+    void COBSREncodingCheck128( void );
+    COBSREncodingCheck128();
+    COBSRLoopCheck();
+    // TriceWrite8( id(0), "%d:LED %d", addr, 1); // experimental send command
+}
+
