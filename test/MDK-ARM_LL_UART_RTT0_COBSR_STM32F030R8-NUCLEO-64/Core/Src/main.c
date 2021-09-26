02d716bf36a7744f4c74a9d5676d56293bb128a1
Thomas Höhenleitner <82028363+th-sabik@users.noreply.github.com>
2021-09-26 13:02:32 +0200

    prepared for debugging

--- test/MDK-ARM_LL_UART_RTT0_COBSR_STM32F030R8-NUCLEO-64/Core/Src/main.c
+++ test/MDK-ARM_LL_UART_RTT0_COBSR_STM32F030R8-NUCLEO-64/Core/Src/main.c
@@ -22,9 +22,8 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include <stdlib.h>
+#include <stdlib.h> // srand
 #include "trice.h"
-
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -97,7 +96,8 @@ int main(void)
   MX_USART2_UART_Init();
   /* USER CODE BEGIN 2 */
     LL_USART_EnableIT_RXNE(TRICE_UART); // enable UART2 interrupt
-    TRICE_HEADLINE;
+    //TRICE_HEADLINE;
+    //TRICE16_1( Id( 44374), "tst:Hi! %x\n", 0x1122 );
     srand(SYSTICKVAL16);
   /* USER CODE END 2 */
 
@@ -108,15 +108,22 @@ int main(void)
     static int lastTricesTime = 0;
     { // send some trices every few ms
         if( milliSecond >= lastTricesTime + 1000 ){
-            static int index = 0;
-            int select = index % 32;
-            #if TRICE_FLEX_ENCODING == TRICE_ENCODING
-            TRICE16_2( Id(1047663),"MSG: triceFifoMaxDepth = %d, select = %d\n", triceFifoMaxDepth, select ); // bigID check
-            #else
-            TRICE16_2( Id(47663),"MSG: triceFifoMaxDepth = %d, select = %d\n", triceFifoMaxDepth, select );
-            #endif
-            triceCheckSet(index);
-            index++;
+          //TRICE0( Id( 48738), "tst:Hi!\n" );                     // [33 230 0]        ...  [34 230 0]
+          //TRICE8_1( Id( 36005), "tst:Hi! %d\n", 1 );             // [4 202 81 1 0]    ...  [4 202 82 1 0]
+          //TRICE8_2( Id( 53180), "tst:Hi! %d %d\n", 1, 2 );       // [5 251 195 1 2 0] ...  [5 251 196 1 2 0]
+            TRICE8_3( Id( 37316), "tst:Hi! %d %d %d\n", 1, 2, 3 ); // [6 28 67 1 2 3 0] ...  [6 28 68 1 2 3 0]
+          //TRICE16_1( Id( 44374), "tst:Hi! %x\n", 0x1213 );       // [18 213 103 19 0] ...  [18 213 103 19 0]
+
+
+            //static int index = 0;
+            //int select = index % 2;
+            //#if TRICE_FLEX_ENCODING == TRICE_ENCODING
+            //TRICE16_2( Id(1047663),"MSG: triceFifoMaxDepth = %d, select = %d\n", triceFifoMaxDepth, select ); // bigID check
+            //#else
+            //TRICE16_2( Id(47663),"MSG: triceFifoMaxDepth = %d, select = %d\n", triceFifoMaxDepth, select );
+            //#endif
+            //triceCheckSet(select);
+            //index++;
             lastTricesTime = milliSecond;
         }
     }
