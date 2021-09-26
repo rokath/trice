88ce4bf5596d22618776b6f6bfaa7f7c0b2c2de8
Thomas Höhenleitner <82028363+th-sabik@users.noreply.github.com>
2021-09-26 09:31:52 +0200

    minor typo

--- test/MDK-ARM_LL_UART_RTT0_ESC_STM32F030R8_NUCLEO-64/Core/Src/main.c
+++ test/MDK-ARM_LL_UART_RTT0_ESC_STM32F030R8_NUCLEO-64/Core/Src/main.c
@@ -306,7 +306,7 @@ void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
   /* User can add his own implementation to report the file name and line number,
-     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
