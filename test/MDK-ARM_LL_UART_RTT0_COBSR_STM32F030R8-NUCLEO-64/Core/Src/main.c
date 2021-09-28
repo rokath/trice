ad2e15c136e3cdf7ff4991c215590429d67fa9fa
Thomas Höhenleitner <th@seerose.net>
2021-09-28 08:18:57 +0200

    Update main.c

--- test/MDK-ARM_LL_UART_RTT0_COBSR_STM32F030R8-NUCLEO-64/Core/Src/main.c
+++ test/MDK-ARM_LL_UART_RTT0_COBSR_STM32F030R8-NUCLEO-64/Core/Src/main.c
@@ -68,7 +68,7 @@ static uint8_t cycle = 80; //!< trice cycle counter
 
 
 //! triceRead returns a pointer to next complete trice message or it returns NULL if no data to process.
-//! If in a first try the buffer is empty a buffer swap is done:
+//! If in a first try the read buffer is empty a buffer swap is done:
 //! \li Switch next write to this buffer here, because it is empty.
 //! \li Switch next read to the other buffer, may be there is stuff to read.
 //! If both buffers empty each triceRead call results in a buffer swap, what is ok.
