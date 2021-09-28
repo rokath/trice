f138256a74c061e28e3232cb19c686dc714cca03
Thomas Höhenleitner <th@seerose.net>
2021-09-28 23:01:19 +0200

    Update triceCobsrEncoder.h

--- pkg/src/intern/triceCobsrEncoder.h
+++ pkg/src/intern/triceCobsrEncoder.h
@@ -16,6 +16,7 @@ extern "C" {
 // 8 bit count 0-255
 
 #define Id(n) ((uint32_t)n<<16)
+#define TRICE_CYCLE_START 0xC0
 #define TRICE_CYCLE TRICE_CYCLE_START // TriceCycle++
 
 //! TRICE0 writes trice data as fast as possible in a buffer.
