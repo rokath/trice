587bd645f0e6702bd3a24052f9b554036f206b1b
Thomas Höhenleitner <th@seerose.net>
2022-03-22 10:33:41 +0100

    TCOBS coding WIP

--- pkg/tcobs/TCOBS.c
+++ pkg/tcobs/TCOBS.c
@@ -10,7 +10,7 @@
 #define OUTB( b ) do{ *o++ = b; offset++; if( offset == 31 ){ *o++ = N | 31; offset = 0; } } while( 0 ); //!< OUTB writes a non-sigil byte to output.
 
 
-#define OUT_zeroSigil do{ ASSERT( b_1 == 0    ); ASSERT( fullCount|reptCount == 0 ); ASSERT( 0 < zeroCount && zeroCount < 4 );                                                    *o++ =        (zeroCount    << 5) | offset; offset = 0; zeroCount = 0; }while(0)
+#define OUT_zeroSigil do{ ASSERT( b_1 == 0    ); ASSERT( fullCount|reptCount == 0 ); ASSERT(                  zeroCount < 4 );                                                    *o++ =        (zeroCount    << 5) | offset; offset = 0; zeroCount = 0; }while(0)
 #define OUT_fullSigil do{ ASSERT( b_1 == 0xFF ); ASSERT( zeroCount|reptCount == 0 ); ASSERT( 1 < fullCount && fullCount < 5 );                                                    *o++ = 0x80 | (fullCount    << 5) | offset; offset = 0; fullCount = 0; }while(0)
 #define OUT_reptSigil do{                        ASSERT( zeroCount|fullCount == 0 ); ASSERT( 1 < reptCount && reptCount < 5 ); if( offset > 7 ){ *o++ = N | offset; offset = 0; } *o++ =        (reptCount-1) << 3) | offset; offset = 0; reptCount = 0; }while(0)
 
@@ -39,66 +39,72 @@ int TCOBSEncode( uint8_t* restrict output,  uint8_t const * restrict input, unsi
                        //                     1-7 for xx and R5
     uint8_t b_1 = 0; // previous byte
     uint8_t b = 0; // current byte
-    if( length == 0 ){ // nothing to do
-        return 0;
-    }
-    if( length == 1 ){ // simple special case
-        b = *i;
-        if( b == 0 ){
-            *o = Z1;
-            return 1;
-        }else{
-            *o++ = b;
-            *o = N | 1;
-            return 2;
+
+    if( length < 3 ){ // special cases
+        if( length == 0 ){ 
+            return 0;
+        }
+        if( length == 1 ){
+            b = *i;
+            if( b == 0 ){
+                *o = Z1;
+                return 1;
+            }else{
+                *o++ = b;
+                *o = N | 1;
+                return 2;
+            }
+        }
+        if( length == 2 ){ 
+            b_1 = *i++;
+            b = *i++;
+            if( (b_1 | b) == 0 ){
+                *o++ = Z2;
+                return 1;
+            } else if( b_1 == 0){
+                *o++ = Z1;
+                *o++ = b;
+                *o++ = N | 1;
+                return 3;
+            } else if( b == 0 ){
+                *o++ = b_1;
+                *o++ = Z1 | 1;
+                return 2;
+            } else {
+                *o++ = b_1;
+                *o++ = b;
+                *o++ = N | 2;
+                return 3;
+            }
         }
     }
-    if( length == 2 ){
-        b_1 = *i++;
-        b = *i++;
-        if( (b_1 | b) == 0 ){
-            *o++ = Z2;
-            return 1;
-        } else if( b_1 == 0){
-            *o++ = Z1;
-            *o++ = b;
-            *o++ = N | 1;
-            return 3;
-        } else if( b == 0 ){
-            *o++ = b_1;
-            *o++ = Z1 | 1;
-            return 2;
-        } else {
-            *o++ = b_1;
-            *o++ = b;
-            *o++ = N | 2;
-            return 3;
-        }
-    }
+
+    // length >= 3
     b = *i++; // get first byte
     for(;;){
         b_1 = b; // keep byte for compare
         b = *i++; // get next byte
-        if( limit - i > 0 ){ // most likely
+
+        if( limit - i > 0 ){ // most likely case, loop continuation
             if( b_1|b == 0){ // both 0         
                 zeroCount++;
-                if( zeroCount == 3 ){ // 00 00 00 00
+                if( zeroCount == 3 ){ // , 00 00 00 00
                     OUT_zeroSigil 
                 } // Z3, 00
                 continue;
             }
             if( b_1 & b == 0xFF ){ // both FF    
                 fullCount++;
-                if( fullCount == 4 ){ // FF FF FF FF FF
+                if( fullCount == 4 ){ // , FF FF FF FF FF
                     OUT_fullSigil
                 } // F4, FF
                 continue;                
             }
             if( b_1 == b  ){ // both equal      
                 reptCount++;
-                if( reptCount == 5 ){ // xx xx xx xx xx xx
+                if( reptCount == 5 ){ // , xx xx xx xx xx xx
                     *o++ = b_1;
-                    offset++:
+                    offset++: // xx, xx xx xx xx xx
                     OUT_reptSigil
                 } // xx R4, xx
                 continue;                
@@ -121,140 +127,101 @@ int TCOBSEncode( uint8_t* restrict output,  uint8_t const * restrict input, unsi
                 OUT_reptSigil // xx Rn,
                 continue;
 
-            // at this point all counts 0, b_1 != b and b_1 == 00 || FF || xx
+            // at this point all counts 0, b_1 != b and b_1 == 00 || FF || xx, b == 00 || FF || yy
 
-            if( b_1 == 0 ){
-                if( fullCount == 1 ){
-                    *o++ = 0xFF;
-                    fullCount = 0;
-                    offset++;
-                    if( offset == 31 ){
-                        *o++ = N | 31;
-                        offset = 0;
-                    }
-                }else if( fullCount > 1 )
-                    ASSERT( fullCount < 5 )
-                    *o++ = 0x80 | (fullCount<<5) | offset;
-                    offset = 0;
-                }else switch( repeatCount ){
-                    // ?
-                }
-                zeroCount++; 
-                if( zeroCount < 3 ){
-                    continue;
-                }else{ // 3rd zero
-                    ASSERT( offset < 32 )
-                    *o++ = Z3 | offset; // Z3=011ooooo
-                    offset = 0;
-                    zeroCount = 0;
-                    continue;
-                }
-            }else if( b_1 == 0xFF ){
-                fullCount++;
-                if( fullCount < 4 ){
-                    continue;
-                }else{ // 4th FF
-                    ASSERT( offset < 32 )
-                    *o++ = F4 | offset; // F4=0x100ooooo
-                    offset = 0;
-                    fullCount = 0;
-                    continue;
-                }
-            }else{ // b = 01...FE
-                if( zeroCount ){ // Z1=001ooooo, Z2=010ooooo, Z3=011ooooo
-                    ASSERT( offset < 32 && fullCount == 0 && reptCount == 0 )
-                    *o++ = (zeroCount << 5) | offset;
-                    offset = 1;
-                    zeroCount = 0;
-                    *o++ = b_1; 
-                    continue;
-                } else if( fullCount == 1 ){ // a single FF
-                    ASSERT( offset < 32 && zeroCount == 0 && reptCount == 0 )
-                    OUTB( 0xFF )
-                    fullCount = 0;
-                    OUTB( b_1 )
-                    continue;
-                }else if( 2 <= fullCount && fullCount <= 4 ){ // 2-4 FF
-                    ASSERT( offset < 32 && zeroCount == 0 && reptCount == 0 )
-                    *o++ = 0x80 | (fullCount << 5) | offset;
-                    offset = 1;
-                    fullCount = 0;
-                    *o++ = b_1;
-                    continue;
-                }else if( b_1 != b ){
-                    ASSERT( offset < 32 && zeroCount == 0 && fullCount == 0 )
-                    switch( reptCount ){
-                    case 0: // no repetition
-                        OUTB( b_1 )
-                        continue;
-                    case 1: // one repetition
-                        OUTB( b_1 )
-                        reptCount = 0;
-                        continue;
-                    case 2: // two repetitions
-                        ASSERT( offset < 32 )
-                        if( offset > 7 ){
-                            *o++ = N | offset;
-                            offset = 0;
-                        }
-                        *o++ = R2 | offset; // R2=0x00001ooo
-                        offset = 1;
-                        *o++ = b_1; 
-                        reptCount = 0;
-                        continue; 
-                    case 3: // 3 repetitions
-                        ASSERT( offset < 32 )
-                        if( offset > 7 ){
-                            *o++ = N | offset;
-                            offset = 0;
-                        }
-                        *o++ = R3 | offset; // R3=0x00010ooo
-                        offset = 1;
-                        *o++ = b_1;
-                        reptCount = 0;
-                        continue; 
-                    case 4: // 4 repetitions
-                        ASSERT( offset < 32 )
-                        if( offset > 7 ){
-                            *o++ = N | offset;
-                            offset = 0;
-                        }
-                        *o++ = R4 | offset; // R4=0x00011ooo
-                        offset = 1;
-                        *o++ = b_1;
-                        reptCount = 0;
-                        continue; 
-                    case 5: // 5 repetitions
-                        ASSERT( offset < 32 )
-                        if( offset > 6 ){
-                            *o++ = N | offset;
-                            offset = 0;
-                        }
-                        *o++ = R5 | (offset+1); // R5=0x00000ooo
-                        offset = 1;
-                        *o++ = b_1;
-                        reptCount = 0;
-                        continue; 
-                    default:
-                       ASSERT( 0 )
-                    }
-                }else{ // b_1 == b
-                    reptCount++;
-                    if( reptCount < 5 ){
-                        continue;
-                    }
-                    ASSERT( offset < 32 )
-                    if( offset > 6 ){
-                        *o++ = N | offset;
-                        offset = 0;
-                    }
-                    *o++ = R5 | (offset+1); // R5=0x00000ooo
-                    offset = 0;
-                    reptCount = 0;
-                    continue;
+            if( b_1 == 0 ){ // , 00 xx
+                OUT_zeroSigil // Z1, xx
+                continue;
+            }
+            if( b_1 == 0xFF ){ // , FF xx
+                OUTB( 0xFF ); // FF, xx
+                continue;
+            }
+            // , b_1 = 01...FE 
+            OUTB( b_1 ) // b_1,
+            continue;
+
+        }else{ // last 2 bytes
+
+        counts sind nicht ganz klar
+
+            if( b_1 | b == 0 ) { // Zn 00 00
+                switch( zeroCount ){
+                case 0: // , 00 00
+                    zeroCount = 2;
+                    OUT_zeroSigil // Z2,
+                    return o - output;
+                case 1: // , Z1 00 00
+                    zeroCount = 3;
+                    OUT_zeroSigil // Z3, 
+                    return o - output;
+                case 2: // , Z1 00 00
+                    zeroCount = 3;
+                    OUT_zeroSigil // Z3, 00
+                    *o++ = Z1; // Z3 Z1,
+                    return o - output;
+                default:
+                    ASSERT(0)
+                    return 0;
                 }
             }
-        }else{ // last 2 bytes
+
+            if( b_1 == 0 ){ // , Zn 00 xx
+                switch( zeroCount ){
+                case 0: // , 00 xx
+                    zeroCount = 2;
+                    OUT_zeroSigil // Z2,
+                    return o - output;
+                case 1: // , Z1 00 00
+                    zeroCount = 3;
+                    OUT_zeroSigil // Z3, 
+                    return o - output;
+                case 2: // , Z1 00 00
+                    zeroCount = 3;
+                    OUT_zeroSigil // Z3, 00
+                    *o++ = Z1; // Z3 Z1,
+                    return o - output;
+                default:
+                    ASSERT(0)
+                    return 0;
+                }
+
+            }
+
+
+            }
+            if( zeroCount && ((b_1|b)==0) ){ // , Zn 00 00
+                zeroCount++;
+                if( )
+
+
+
+            if( zeroCount ) // 00 b_1 b
+                zeroCount++;
+                OUT_zeroSigil // Zn,
+                continue;
+            if( fullCount ) // FF
+                fullCount++;
+                OUT_fullSigil // Fn,
+                continue;
+            if( reptCount ) // xx
+                *o++ = xx;
+                offset++;
+                OUT_reptSigil // xx Rn,
+                continue;
+
+
+
+
+
+
+
+
+
+
+
+
+
         // zeroCount > 0 means 0 occurred zeroCount times but not written yet
         // fullCount > 0 means 0xFF occurred fullCount times but not written yet
         // reptCount > 0 means b_1 occurred reptCount times but not written yet
