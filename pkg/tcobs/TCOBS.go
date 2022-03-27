76b72c0b5f9acd5a172cdf7cdc248434a1f59d04
Thomas Höhenleitner <th@sabik-offshore.com>
2022-03-27 11:17:39 +0200

    wip

--- pkg/tcobs/TCOBS.go
+++ pkg/tcobs/TCOBS.go
@@ -14,6 +14,7 @@ import (
 )
 
 // TCOBSEncodeC encodes i into o and returns number of bytes in o.
+// For details see TCOBSSpecification.md.
 func TCOBSEncodeC(o, i []byte) (n int) {
 	if len(i) == 0 {
 		return
@@ -25,21 +26,22 @@ func TCOBSEncodeC(o, i []byte) (n int) {
 }
 
 const (
-	N        = 0xA0
-	Z1       = 0x20
-	Z2       = 0x40
-	Z3       = 0x60
-	F2       = 0xC0
-	F3       = 0xE0
-	F4       = 0x80
-	R2       = 0x08
-	R3       = 0x10
-	R4       = 0x18
-	Reserved = 0x00
+	N        = 0xA0 // noop sigil byte
+	Z1       = 0x20 // 1-zero sigil byte
+	Z2       = 0x40 // 2-zero sigil byte
+	Z3       = 0x60 // 3-zero sigil byte
+	F2       = 0xC0 // 2-FF sigil byte
+	F3       = 0xE0 // 3-FF sigil byte
+	F4       = 0x80 // 4-FF sigil byte
+	R2       = 0x08 // 2-repeat sigil byte
+	R3       = 0x10 // 3-repeat sigil byte
+	R4       = 0x18 // 4-repeat sigil byte
+	Reserved = 0x00 // reserved sigil byte
 )
 
-// sigilAndOffet
-func sigilAndOffet(by uint8) (sigil, offset int) {
+// sigilAndOffset interprets by as sigil byte with offset and returns sigil and offset.
+// For details see TCOBSSpecification.md.
+func sigilAndOffset(by uint8) (sigil, offset int) {
 	b := int(by)
 	sigil = b & 0xE0
 	if sigil == 0 {
@@ -55,26 +57,22 @@ func sigilAndOffet(by uint8) (sigil, offset int) {
 // ATTENTION: d is filled from the end! decoded:= d[len(d)-n:] is the final result.
 // In case of an error n is 0. n can be 0 without having an error.
 // Framing with 0-delimiter to be done before is assumed, so no 0-checks performed.
+// For details see TCOBSSpecification.md.
 func TCOBSDecode(d, in []byte) (n int, e error) {
 	for {
 		if len(in) == 0 {
 			return
 		}
-                next := in[len(in)-1]
-		sigil, offset := sigilAndOffet(next)
+		next := in[len(in)-1] // get next sigil byte (starting from the end)
+		sigil, offset := sigilAndOffset(next)
 		if offset+1 > len(in) {
 			e = errors.New("sigil chain mismatch")
 			return
 		}
-		in = in[:len(in)-1] // remove sigil byte
+		in = in[:len(in)-1] // remove sigil byte from buffer
 		switch sigil {
 		case N:
-copyBytes:
-		        to:= len(d)-n-offset-1
-                        from := len(in)-offset-1
-                        n += copy(d[to], in[from], offset)
-			in = in[:len(in)-offset]
-			continue
+			goto copyBytes
 
 		case Z3:
 			n++
@@ -115,23 +113,33 @@ copyBytes:
 			d[len(d)-n] = in[len(in)-1]
 			n++
 			d[len(d)-n] = in[len(in)-1]
-		        n++
-			d[len(d)-n] = in[len(in)-1]	
-                        in = in[:len(in)-1]
+
+			n++
+			d[len(d)-n] = in[len(in)-1]
+			in = in[:len(in)-1] // remove repeated data byte
 			goto copyBytes
 
 		case Reserved:
+			n = 0
 			e = errors.New("reserved sigil not allowed")
 			return
 		}
+
+	copyBytes:
+		to := len(d) - n - offset - 1
+		from := len(in) - offset - 1
+		n += copy(d[to:], in[from:])
+		in = in[:len(in)-offset] // remove copied bytes
+		continue
 	}
 }
 
 // TCOBSDecoder expects in s a 0-delimited number of TCOBS packages. If a 0-delimiter is found inside s,
 // it tries to decode all bytes until the 0-delimiter as TCOBS package and writes the result in decoded.
-// no is the number of valid data in o afterwards and ni is the number of bytes removed from i (including the 0-delimiter).
-// If s does not contain any 0-delimiter decoded is nil, after is s and e is nil.
-// If a 0-delimiter was found, but the TCOBS decoding failed, no is 0 and e is not nil.
+// If s does not contain any 0-delimiter, decoded is nil, after is s and e is nil.
+// If a 0-delimiter was found, after contains all data after the found delimiter.
+// If the TCOBS decoding of the data until the 0-delimiter failed, decoded has len 0, and e is not nil.
+// For details see TCOBSSpecification.md.
 func TCOBSDecoder(s []byte) (decoded, after []byte, e error) {
 	tcobs, after, found := bytes.Cut(s, []byte{0})
 	if !found {
