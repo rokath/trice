1af831afe7fc9b97ef83fed0a13d5987c7c7e812
rokath <th@seerose.net>
2022-08-11 15:37:21 +0200

    comment changed

--- pkg/src/inc/triceConfig.h
+++ pkg/src/inc/triceConfig.h
@@ -17,8 +17,8 @@ extern "C" {
 
 #define TRICE_MODE 0 //! TRICE_MODE is a predefined trice transfer method.
 
-//#define TRICE_RTT_CHANNEL 0 //!< Enable and set channel number for SeggerRTT usage. Only channel 0 works right now for some reason.
-//#define TRICE_UART USART2   //!< Enable and set UART for serial output.
+//#define TRICE_RTT_CHANNEL 0 //!< Uncomment and set channel number for SeggerRTT usage.
+//#define TRICE_UART USART2   //!< Uncomment and set UART for serial output.
 #define TRICE_CGO_TEST
 
 
