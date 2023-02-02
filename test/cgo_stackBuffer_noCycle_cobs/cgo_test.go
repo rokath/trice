541cfe1b7167feab552b0e3ccd40dd4cc0c6bc7a
Tea Age <gitkraken@seerose.net>
2023-02-02 20:59:18 +0100

    wip cgo test improved

--- test/cgo_stackBuffer_noCycle_cobs/cgo_test.go
+++ test/cgo_stackBuffer_noCycle_cobs/cgo_test.go
@@ -12,35 +12,24 @@ import (
 	"github.com/tj/assert"
 )
 
+// triceLog logs binary trice data inside fileName and return output as string
+func triceLog(t *testing.T, fSys *afero.Afero, fileName string) string {
+	var o bytes.Buffer
+	assert.Nil(t, args.Handler(io.Writer(&o), fSys, []string{"trice", "log", "-i", "til.json", "-p", "FILEBUFFER", "-args", "fileBuffer.bin", "-packageFraming", "COBS", "-ts", "off", "-prefix", "off", "-tsf", "", "-li", "off", "-color", "off"}))
+	return o.String()
+}
+
 func TestTriceCheck(t *testing.T) {
 
 	osFSys := &afero.Afero{Fs: afero.NewOsFs()}
 	mmFSys := &afero.Afero{Fs: afero.NewMemMapFs()}
 	wd := "d:/repos/trice/test/"
 
-	// trice update testdata
-	//var b bytes.Buffer
-	//assert.Nil(t, args.Handler(io.Writer(&b), osFSys, []string{"trice", "u", "-i", wd + "testdata/generated_til.json", "-src", wd + "cgo_stackBuffer_noCycle_cobs"}))
-
-	// prepare
-
-	//cgot.CopyFileIntoFSys(t, mmFSys, "triceCheck.c", osFSys, wd+"testdata/generated_triceCheck.c") // needed for the expected results
 	CopyFileIntoFSys(t, mmFSys, "til.json", osFSys, wd+"testdata/til.json") // needed for the trice log
-
 	out := make([]byte, 32768)
 	SetTriceBuffer(out)
 
 	result := GetExpectedResults(osFSys, wd+"testdata/generated_triceCheck.c")
-
-	// show generated til.json for debugging
-	//tBytes, e := osFSys.ReadFile(wd + "testdata/til.json")
-	//assert.Nil(t, e)
-	//fmt.Println(string(tBytes))
-
-	// show modified "triceCheck.c" for debugging
-	//cBytes, e := osFSys.ReadFile(wd + "testdata/generated_triceCheck.c")
-	//assert.Nil(t, e)
-	//fmt.Println(string(cBytes))
 
 	for i, r := range result {
 
@@ -51,14 +40,10 @@ func TestTriceCheck(t *testing.T) {
 		length := TriceOutDepth()
 		bin := out[:length] // bin contains the binary trice data of trice message i
 
-		fmt.Println(i, bin) // // show data for debugging
+		//fmt.Println(i, bin) // // show data for debugging
 		assert.Nil(t, mmFSys.WriteFile("fileBuffer.bin", bin, 0777))
 
-		// trice log
-		var o bytes.Buffer
-		assert.Nil(t, args.Handler(io.Writer(&o), mmFSys, []string{"trice", "log", "-i", "til.json", "-p", "FILEBUFFER", "-args", "fileBuffer.bin", "-packageFraming", "COBS", "-ts", "off", "-prefix", "off", "-tsf", "", "-li", "off", "-color", "off"}))
-
-		act := o.String()
+		act := triceLog(t, mmFSys, "fileBuffer.bin")
 		assert.Equal(t, r.Exp, strings.TrimSuffix(act, "\n"))
 	}
 }
