5e513a415573fb4ed38b1a737adf91fe2a9cb628
Thomas Höhenleitner <82028363+th-sabik@users.noreply.github.com>
2023-04-04 18:11:09 +0200

    gdbserver wip

--- test/testdata/jlinkgdbserver.sh
+++ test/testdata/jlinkgdbserver.sh
@@ -1,18 +1,25 @@
-JLinkGDBServer \
+JLinkGDBServerCL \
 -device STM32F030R8 \
--endian little \
 -if SWD \
 -speed 4000 \
--nolocalhostonly \
--port 3333 \
--telnetport 2333 \
--noir \
--nohalt \
 -notimeout \
--noreset \
--select USB \
--strict \
--RTTTelnetPort 4444 \
+
+
+# JLinkGDBServer \
+# -device STM32F030R8 \
+# -endian little \
+# -if SWD \
+# -speed 4000 \
+# -nolocalhostonly \
+# -port 3333 \
+# -telnetport 2333 \
+# -noir \
+# -nohalt \
+# -notimeout \
+# -noreset \
+# -select USB \
+# -strict \
+# -RTTTelnetPort 4444 \
 
 # -settingsfile Selects the J-Link Settings File.
 # -jlinkscriptfile “C:\My.JLinkScript” (7.13)
