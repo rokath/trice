a0a39183c6c26072abe4154543c54222a63f88e4
Thomas Höhenleitner <th@seerose.net>
2023-05-16 10:29:51 +0200

    v0.60.2

--- test/MDK-ARM_STM32F030R8_instrumented/Core/Inc/triceConfig.h
+++ test/MDK-ARM_STM32F030R8_instrumented/Core/Inc/triceConfig.h
@@ -11,11 +11,11 @@ extern "C" {
 
 //! TriceStamp16 returns a 16-bit value to stamp `Id` TRICE macros. Usually it is a timestamp, but could also be a destination address or a counter for example.
 //! The user has to provide this function. Defining a macro here, instead if providing `int16_t TriceStamp16( void );` has significant speed impact.
-#define TriceStamp16() (0x1616) //(SysTick->VAL) 
+#define TriceStamp16() (SysTick->VAL) 
 
 //! TriceStamp32 returns a 32-bit value to stamp `ID` TRICE macros. Usually it is a timestamp, but could also be a destination address or a counter for example.
 //! The user has to provide this function. Defining a macro here, instead if providing `int32_t TriceStamp32( void );` has significant speed impact.
-#define TriceStamp32() (0x32323232) //Us32()
+#define TriceStamp32() Us32()
 
 //!  TRICE_BUFFER selects, where the TRICE macros accumulate the trice data during a single TRICE execution. Selectable options:
 //! - TRICE_STACK_BUFFER: No additional buffer is needed, what makes sense for single task systems with direct output only.
