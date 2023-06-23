c88848ff61714ba89b8c96e28ee6335729c160f1
Thomas Höhenleitner <th@seerose.net>
2023-06-23 22:50:10 +0200

    universal ID action prepared

--- internal/id/insertIDs.go
+++ internal/id/insertIDs.go
@@ -9,136 +9,18 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"path/filepath"
-	"sync"
 
-	"github.com/rokath/trice/pkg/msg"
 	"github.com/spf13/afero"
 )
 
-// insertIDsData holds the insert run specific data.
-type insertIDsData struct {
-	idToFmt    TriceIDLookUp     // idToFmt is a trice ID lookup map and is generated from existing til.json file at the begin of SubCmdIdInsert. This map is only extended during SubCmdIdInsert and goes back into til.json afterwards.
-	fmtToId    triceFmtLookUp    // fmtToId is a trice fmt lookup map (reversed idToFmt for faster operation) and kept in sync with idToFmt. Each fmt can have several trice IDs (slice).
-	idToLocRef TriceIDLookUpLI   // idToLocInf is the trice ID location information as reference generated from li.json (if exists) at the begin of SubCmdIdInsert and is not modified at all. At the end of SubCmdIdInsert a new li.json is generated from itemToId.
-	itemToId   TriceItemLookUpID // itemToId is a trice item lookup ID map, extended from source tree during SubCmdIdInsert after each found and maybe modified trice item.
-	idToItem   TriceIDLookupItem // idToItem is a trice ID lookup item map (reversed itemToId for faster operation) and kept in sync with itemToId.
-	wg         sync.WaitGroup    // for parallel processing
-}
-
 // SubCmdIdInsert performs sub-command insert, adding trice IDs to source tree.
 func SubCmdIdInsert(w io.Writer, fSys *afero.Afero) error {
-	var iid insertIDsData
-
-	// get state
-	iid.idToFmt = NewLut(w, fSys, FnJSON)
-	iid.fmtToId = iid.idToFmt.reverseS()
-	oLen := len(iid.idToFmt)
-	iid.idToLocRef = make(TriceIDLookUpLI, 4000)
-
-	// prepare
-	iid.itemToId = make(TriceItemLookUpID, 4000)
-	iid.idToItem = make(TriceIDLookupItem, 4000)
-	if len(Srcs) == 0 {
-		Srcs = append(Srcs, "./") // default value
-	}
-
-	// processing Srcs list ...
-	for _, path := range Srcs {
-		if _, err := fSys.Stat(path); err == nil { // path exists
-			dir := path
-			iid.wg.Add(1)
-			go func() {
-				defer iid.wg.Done()
-				idsInsert(w, fSys, dir, &iid) // processing folders parallel
-			}()
-		} else if os.IsNotExist(err) { // path does *not* exist
-			fmt.Fprintln(w, path, "does not exist!")
-		} else {
-			fmt.Fprintln(w, path, "Schrodinger: file may or may not exist. See err for details.")
-			// Therefore, do *NOT* use !os.IsNotExist(err) to test for file existence
-			// https://stackoverflow.com/questions/12518876/how-to-check-if-a-file-exists-in-go
-		}
-	}
-
-	// ...waiting
-	iid.wg.Wait()
-
-	// finalize
-	if Verbose {
-		fmt.Fprintln(w, len(iid.idToFmt), "ID's in List", FnJSON)
-	}
-	if (len(iid.idToFmt) != oLen) && !DryRun {
-		msg.FatalOnErr(iid.idToFmt.toFile(fSys, FnJSON))
-	}
-	if LIFnJSON == "off" || LIFnJSON == "none" {
-		return nil
-	}
-	li := iid.idToLocRef // todo: derive from iid.idToItem
-	return li.toFile(fSys, LIFnJSON)
-}
-
-// idsInsert is passed as argument of walkSrcsInsert, called by SubCmdIdInsert and this way called for each path in Srcs.
-// The passed root is a folder with sub-folders and files or just a single file.
-func idsInsert(w io.Writer, fSys *afero.Afero, root string, piid *insertIDsData) {
-	if Verbose && FnJSON != "emptyFile" {
-		fmt.Fprintln(w, "dir=", root)
-		fmt.Fprintln(w, "List=", FnJSON)
-	}
-
-	// Here the trees, stored in Srcs are walked.
-	msg.FatalInfoOnErr(fSys.Walk(root, visitInsert(w, fSys, piid)), "failed to walk tree") // todo: check fSys.WalkTree for more speed
-}
-
-// visitInsert is passed to fSys.Walk and executed for each file found in the processed root folder.
-// To speed processing up, for each file a go routine is started. (todo)
-// Error handling is done through channels.
-func visitInsert(w io.Writer, fSys *afero.Afero, piid *insertIDsData) filepath.WalkFunc {
-	// WalkFunc is the type of the function called for each file or directory
-	// visited by Walk. The path argument contains the argument to Walk as a
-	// prefix; that is, if Walk is called with "dir", which is a directory
-	// containing the file "a", the walk function will be called with argument
-	// "dir/a". The info argument is the os.FileInfo for the named path.
-	//
-	// If there was a problem walking to the file or directory named by path, the
-	// incoming error will describe the problem and the function can decide how
-	// to handle that error (and Walk will not descend into that directory). In the
-	// case of an error, the info argument will be nil. If an error is returned,
-	// processing stops. The sole exception is when the function returns the special
-	// value SkipDir. If the function returns SkipDir when invoked on a directory,
-	// Walk skips the directory's contents entirely. If the function returns SkipDir
-	// when invoked on a non-directory file, Walk skips the remaining files in the
-	// containing directory.
-	return func(path string, fileInfo os.FileInfo, err error) error {
-
-		if err != nil || fileInfo.IsDir() || !isSourceFile(fileInfo) {
-			return err // forward any error and do nothing
-		}
-
-		piid.wg.Add(1)
-		go func() {
-			defer piid.wg.Done()
-			triceIDInsertion( // processing files parallel
-				w,        // messages destination
-				fSys,     // file system
-				path,     // file name
-				fileInfo, // file type
-				piid)     // pointer to ID insertion data
-		}()
-
-		return nil
-	}
-
+	return ActionCmd(w, fSys, triceIDInsertion)
 }
 
 // triceIDInsertion does the real ID insertion task.
 // For reference look into file TriceUserGuide.me part "The `trice insert` Algorithm".
-func triceIDInsertion(
-	w io.Writer, // messages destination
-	fSys *afero.Afero, // file system
-	path string, // file name
-	fileInfo os.FileInfo, // fileInfo is needed to pass the permissions when opening source files - could probably optimized away.
-	piid *insertIDsData) {
+func triceIDInsertion(w io.Writer, fSys *afero.Afero, path string, fileInfo os.FileInfo, idd *idData) {
 
 	read, err := fSys.ReadFile(path)
 	if nil != err {
@@ -156,7 +38,7 @@ func triceIDInsertion(
 	///////////////////////////
 	///////////////////////////
 
-	// todo: insert using path && piid
+	// todo: insert action using path && idd
 	fmt.Fprintln(w, "processing", path)
 
 	///////////////////////////
