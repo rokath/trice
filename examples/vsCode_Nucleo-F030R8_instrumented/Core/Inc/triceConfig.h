608d1660b8f57249238bd35b9ea434ba37302385
Thomas Höhenleitner <th@seerose.net>
2024-07-04 16:38:04 +0200

    wip

--- examples/vsCode_Nucleo-F030R8_instrumented/Core/Inc/triceConfig.h
+++ examples/vsCode_Nucleo-F030R8_instrumented/Core/Inc/triceConfig.h
@@ -26,18 +26,17 @@ extern uint32_t ms32;
 
 
 
-#if 1 // ERRORC...Debug
+#if 1 // ERROR...Debug
 #define TRICE_BUFFER TRICE_RING_BUFFER
 
 // trice l -p jlink -args "-Device STM32F030R8 -if SWD -Speed 4000 -RTTChannel 0" -showID "deb:%04x" -hs off -pw MySecret -pf COBS -d16=true
 #define TRICE_DIRECT_OUTPUT 1
-#define TRICE_DIRECT_XTEA_ENCRYPT 1
-#define TRICE_DIRECT_OUT_FRAMING TRICE_FRAMING_COBS
 #define TRICE_DIRECT_SEGGER_RTT_32BIT_WRITE 1
 
-// trice l -p com4 -d16=false -pf COBS
+// trice l -p com4 -d16=false -pw MySecret -pf COBS
 #define TRICE_DEFERRED_OUTPUT 1
-#define TRICE_DEFERRED_OUT_FRAMING TRICE_FRAMING_TCOBS
+#define TRICE_DEFERRED_XTEA_ENCRYPT 1
+#define TRICE_DEFERRED_OUT_FRAMING TRICE_FRAMING_COBS
 #define TRICE_DEFERRED_UARTA 1
 #endif
 
