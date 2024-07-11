6e32089d88abcf9bc547155b6a53e233e680ed7b
Thomas Höhenleitner <th@seerose.net>
2024-07-11 17:59:33 +0200

    wip

--- examples/G0B1_inst/build.sh
+++ examples/G0B1_inst/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # trice is called not within the Makefile, to guarantee, it is finished before any other job starts.
-trice update -src ./Core -src ../../test/testdata
+trice update -src ../../test/testdata
+trice insert -src ./Core
 make -j $(nproc --all)
--- internal/id/_insertIDsMain_test.go
+++ internal/id/_insertIDsMain_test.go
@@ -18,11 +18,131 @@ type srcFile struct {
 	insertedIDs string
 }
 
+// TestInsertIDsIntoTilJSONFromFileWithEmptyLi ...
+//
+// IDs 1200 & 1201 are exist, so they are expected to go into til.json.
+func TestInsertIDsIntoTilJSONFromFileWithEmptyLi(t *testing.T) {
+	// trice ID list
+	tilJSON := `{
+}`
+
+	// location information
+	liJSON := `{
+}`
+	testSet := []srcFile{
+		// fn:       existing IDs:                unchanged IDs:
+		{"file0.c", `TRice( iD(1200), "Hi!" );`, `TRice( iD(1200), "Hi!" );`},
+		{"file1.c", `TRice( iD(1201), "Lo!" );`, `TRice( iD(1201), "Lo!" );`},
+	}
+	fSys := &afero.Afero{Fs: afero.NewMemMapFs()}
+
+	// create src files
+	for _, k := range testSet {
+		assert.Nil(t, fSys.WriteFile(k.fn, []byte(k.clean), 0777))
+	}
+
+	// create til.json
+	assert.Nil(t, fSys.WriteFile("til.json", []byte(tilJSON), 0777))
+
+	// create li.json
+	assert.Nil(t, fSys.WriteFile("li.json", []byte(liJSON), 0777))
+
+	// action
+	var b bytes.Buffer
+	assert.Nil(t, args.Handler(io.Writer(&b), fSys, []string{"TRICE", "insert", "-src", "."}))
+
+	// check source files
+	for _, k := range testSet {
+		actSrc, e := fSys.ReadFile(k.fn)
+		assert.Nil(t, e)
+		assert.Equal(t, k.insertedIDs, string(actSrc))
+	}
+
+	// check til.json
+	expTil := `{
+	"1200": {
+		"Type": "TRice",
+		"Strg": "Hi!"
+	},
+	"1201": {
+		"Type": "TRice",
+		"Strg": "Lo!"
+	}
+}`
+	actTil, e := fSys.ReadFile("til.json")
+	assert.Nil(t, e)
+	assert.Equal(t, expTil, string(actTil))
+}
+
+// TestInsertIDsIntoTilJSONFromFileWithLi ...
+//
+// IDs 1200 & 1201 are exist, so they are expected to go into til.json.
+func TestInsertIDsIntoTilJSONFromFileWithLi(t *testing.T) {
+	// trice ID list
+	tilJSON := `{
+}`
+
+	// location information
+	liJSON := `{
+		"1200": {
+			"File": "file0.c",
+			"Line": 1
+		},
+		"1201": {
+			"File": "file1.c",
+			"Line": 1
+		}
+	}`
+	testSet := []srcFile{
+		// fn:       existing IDs:                unchanged IDs:
+		{"file0.c", `TRice( iD(1200), "Hi!" );`, `TRice( iD(1200), "Hi!" );`},
+		{"file1.c", `TRice( iD(1201), "Lo!" );`, `TRice( iD(1201), "Lo!" );`},
+	}
+	fSys := &afero.Afero{Fs: afero.NewMemMapFs()}
+
+	// create src files
+	for _, k := range testSet {
+		assert.Nil(t, fSys.WriteFile(k.fn, []byte(k.clean), 0777))
+	}
+
+	// create til.json
+	assert.Nil(t, fSys.WriteFile("til.json", []byte(tilJSON), 0777))
+
+	// create li.json
+	assert.Nil(t, fSys.WriteFile("li.json", []byte(liJSON), 0777))
+
+	// action
+	var b bytes.Buffer
+	assert.Nil(t, args.Handler(io.Writer(&b), fSys, []string{"TRICE", "insert", "-src", "."}))
+
+	// check source files
+	for _, k := range testSet {
+		actSrc, e := fSys.ReadFile(k.fn)
+		assert.Nil(t, e)
+		assert.Equal(t, k.insertedIDs, string(actSrc))
+	}
+
+	// check til.json
+	expTil := `{
+	"1200": {
+		"Type": "TRice",
+		"Strg": "Hi!"
+	},
+	"1201": {
+		"Type": "TRice",
+		"Strg": "Lo!"
+	}
+}`
+	actTil, e := fSys.ReadFile("til.json")
+	assert.Nil(t, e)
+	assert.Equal(t, expTil, string(actTil))
+}
+
 // TestInsertIDsIntoCleanFilesWithTilJSON ...
 //
 // IDs 1200 & 1201 are exist, so they are expected to be restored on the old position.
 // The IDs 1000 & 1001 are newly assigned.
-func TestInsertIDsIntoCleanFilesWithTilJSON(t *testing.T) {
+func _TestInsertIDsIntoCleanFilesWithTilJSON(t *testing.T) {
 	// trice ID list
 	tilJSON := `{
 	"1200": {
@@ -77,7 +197,7 @@ func TestInsertIDsIntoCleanFilesWithTilJSON(t *testing.T) {
 
 }
 
-func TestInsertIDsIntoCleanFilesWithEmptyTilJSON(t *testing.T) {
+func _TestInsertIDsIntoCleanFilesWithEmptyTilJSON(t *testing.T) {
 	testSet := []srcFile{
 		{"file0.c", `TRice( "Hi!" );`, `TRice( iD(1001), "Hi!" );`},
 		{"file1.c", `TRice( "Hi!" );`, `TRice( iD(1000), "Hi!" );`},
@@ -106,7 +226,7 @@ func TestInsertIDsIntoCleanFilesWithEmptyTilJSON(t *testing.T) {
 	}
 }
 
-func TestInsertIDsAndJSON(t *testing.T) {
+func _TestInsertIDsAndJSON(t *testing.T) {
 
 	fSys := &afero.Afero{Fs: afero.NewMemMapFs()}
 
