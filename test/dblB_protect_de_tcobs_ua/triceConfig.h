8792d9516ec01d16a2ec9cb3543e340662f370f0
rokath <th@seerose.net>
2024-07-16 23:31:10 +0200

    TRICE_PROTRCT for double buffer

---
+++ test/dblB_protect_de_tcobs_ua/triceConfig.h
@@ -0,0 +1,30 @@
+/*! \file triceConfig.h
+\author Thomas.Hoehenleitner [at] seerose.net
+*******************************************************************************/
+
+#ifndef TRICE_CONFIG_H_
+#define TRICE_CONFIG_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//#define TRICE_PROTECT 1
+
+#define TRICE_DEFERRED_BUFFER_SIZE 512
+
+//#define TRICE_DATA_OFFSET 4
+//#define TRICE_SINGLE_MAX_SIZE 104
+
+#define TRICE_DEFERRED_OUTPUT 1
+#define TRICE_DEFERRED_UARTA 1
+#define TRICE_UARTA
+
+#define TRICE_CGO 1
+#define TRICE_CYCLE_COUNTER 0
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* TRICE_CONFIG_H_ */
