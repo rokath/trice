cfef7e4a56c0b9b16424c6d211fa56265e090252
Thomas Höhenleitner <th@seerose.net>
2024-07-22 14:13:19 +0200

    Update triceConfig.h

--- examples/F030R8_inst/Core/Inc/triceConfig.h
+++ examples/F030R8_inst/Core/Inc/triceConfig.h
@@ -17,12 +17,9 @@ extern uint32_t ms32;
 #define TriceStamp32  ms32
 #define TRICE_UARTA   USART2
 
-#define TRICE_BUFFER TRICE_RING_BUFFER
-
 // trice l -p jlink -args "-Device STM32F030R8 -if SWD -Speed 4000 -RTTChannel 0" -pf NONE -d16 -showID "deb:%04x"
-#define TRICE_DIRECT_OUTPUT 1
-#define TRICE_DIRECT_OUT_FRAMING TRICE_FRAMING_COBS
-#define TRICE_DIRECT_SEGGER_RTT_8BIT_WRITE 1
+// #define TRICE_DIRECT_OUTPUT 1
+// #define TRICE_DIRECT_SEGGER_RTT_32BIT_WRITE 1
 
 // trice l -p com4 -pf COBS -pw MySecret -showID "deb:%04x"
 #define TRICE_DEFERRED_OUTPUT 1
