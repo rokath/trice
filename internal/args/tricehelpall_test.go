f33ab5a51ec1cdd8c53d2a39a610f7bb8ceb9297
rokath <th@seerose.net>
2024-07-26 12:21:37 +0200

    wip

--- internal/args/tricehelpall_test.go
+++ internal/args/tricehelpall_test.go
@@ -112,7 +112,7 @@ sub-command 'l|log': For displaying trice logs coming from port. With "trice log
     	Use to pass port specific parameters. The "default" value depends on the used port:
     	port "BUFFER": default="0 0 0 0", Option for args is any space separated decimal number byte sequence. Example -p BUFFER -args "7 123 44".
     	port "DUMP": default="", Option for args is any space or comma separated byte sequence in hex. Example: -p DUMP -args "7B 1A ee,88, 5a".
-    	port "COMn": default="", Unused option for a different driver. (For baud rate settings see -baud.)
+    	port "COMn": default="-baud 115200", Unused option for a different driver. (For baud rate settings see -baud.)
     	port "FILE": default="trices.raw", Option for args is any file name for binary log data like written []byte{115, 111, 109, 101, 10}. Trice retries on EOF.
     	port "FILEBUFFER": default="trices.raw", Option for args is any file name for binary log data like written []byte{115, 111, 109, 101, 10}. Trice stops on EOF.
     	port "J-LINK": default="-Device STM32F030R8 -if SWD -Speed 4000 -RTTChannel 0 -RTTSearchRanges 0x20000000_0x1000", 
