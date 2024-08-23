0e4c8c536f654a2ca3ccb551a072374a6a4d5db4
rokath <th@seerose.net>
2024-08-23 21:08:33 +0200

    ++ CompactSrcs, ++ evaluateTriceParameterCount, ++ matchBracketLiteral

--- internal/id/insertIDs.go
+++ internal/id/insertIDs.go
@@ -7,12 +7,15 @@ package id
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"io"
 	"log"
 	"math"
 	"os"
+	"path"
 	"path/filepath"
+	"slices"
 	"strconv"
 	"strings"
 
@@ -20,14 +23,31 @@ import (
 	"github.com/spf13/afero"
 )
 
+// CompactSrcs adds local dir to Srcs if Srcs is empty and reduces variable Scrs to the minimum to address all intended folders.
+func CompactSrcs() {
+	if len(Srcs) == 0 { // Srcs is an array flag containing desired folders & files
+		Srcs = append(Srcs, "./") // default value
+	}
+	for i, x := range Srcs {
+		Srcs[i] = path.Clean(x)
+	}
+	slices.Sort(Srcs)
+	Srcs = slices.Compact(Srcs)
+}
+
+var SkipAdditionalChecks bool
+
 // SubCmdIdInsert performs sub-command insert, adding trice IDs to source tree.
-func SubCmdIdInsert(w io.Writer, fSys *afero.Afero) error {
-	return IDData.cmdSwitchTriceIDs(w, fSys, IDData.triceIDInsertion)
+func SubCmdIdInsert(w io.Writer, fSys *afero.Afero) (e error) {
+	e = IDData.cmdSwitchTriceIDs(w, fSys, IDData.triceIDInsertion)
+	if e != nil {
+		return e
+	}
+	return
 }
 
 // triceIDInsertion reads file, processes it and writes it back, if needed.
 func (p *idData) triceIDInsertion(w io.Writer, fSys *afero.Afero, path string, fileInfo os.FileInfo, a *ant.Admin) error {
-
 	in, err := fSys.ReadFile(path)
 	if err != nil {
 		return err
@@ -97,9 +117,6 @@ func removeIDFromSlice(ids []TriceID, id TriceID) []TriceID {
 // - idInSourceIsZero,    trice is is inside p.triceToId                                      -> unused ID -> use ID (remove from p.triceToId)
 //   - If trice is assigned to several IDs, the location information consulted. If a matching liPath exists, its first occurrence is used.
 func (p *idData) insertTriceIDs(w io.Writer, liPath string, in []byte, a *ant.Admin) (out []byte, modified bool, err error) {
-
-	// w = os.Stdout - for debugging only
-
 	var idn TriceID    // idn is the last found id inside the source.
 	var idN TriceID    // idN is the to be written id into the source.
 	var idS string     // idS is the "iD(n)" statement, if found.
@@ -118,7 +135,13 @@ func (p *idData) insertTriceIDs(w io.Writer, liPath string, in []byte, a *ant.Ad
 		}
 		t.Type = rest[loc[0]:loc[1]]       // t.Type is the TRice8_2 or TRice part for example. Hint: TRice defaults to 32 bit if not configured differently.
 		t.Strg = rest[loc[5]+1 : loc[6]-1] // Now we have the complete trice t (Type and Strg). We remove the double quotes wit +1 and -1.
-		if loc[3] != loc[4] {              // iD(n) found
+		if !SkipAdditionalChecks {
+			err = evaluateTriceParameterCount(t, line, rest[loc[6]:])
+			if err != nil {
+				return
+			}
+		}
+		if loc[3] != loc[4] { // iD(n) found
 			idS = rest[loc[3]:loc[4]] // idS is where we expect n.
 			nLoc := matchNb.FindStringIndex(idS)
 			if nLoc == nil { // Someone wrote trice( iD(0x100), ...), trice( id(), ... ) or trice( iD(name), ...) for example.
@@ -238,11 +261,14 @@ func (p *idData) insertTriceIDs(w io.Writer, liPath string, in []byte, a *ant.Ad
 			}
 		}
 		if Verbose {
-			fmt.Println("If no match was found inside ids we assign the ID found in source file.")
+			fmt.Fprintln(w, "If no match was found inside ids we assign the ID found in source file.")
 		}
 		if idN == 0 {
 			tm, used := p.idToTrice[idn]
 			if !used {
+				if Verbose {
+					fmt.Fprintln(w, "It is possible, that idn is 0, so we assign it to idN", idn, "-->", idN)
+				}
 				idN = idn // It is possible, that idn is 0, for example when `TRICE( id(0), "Hi!" );` is inside src.
 			} else {
 				if tm != t {
@@ -250,7 +276,10 @@ func (p *idData) insertTriceIDs(w io.Writer, liPath string, in []byte, a *ant.Ad
 						fmt.Fprintln(w, "ID", idn, "used in TIL with tm=", tm, " but in src it is used for t=", t, "Force changing the ID.")
 					}
 					if len(ids) > 0 { // At this point we could take an id from ids.
-						idn = 0 // Discard value found in src. (case ID in src already used differently)
+						if Verbose {
+							fmt.Fprintln(w, "Discard value found in src. (case ID in src already used differently):", idn)
+						}
+						idn = 0
 						goto idsLoopEntry
 					}
 				} else {
@@ -261,7 +290,10 @@ func (p *idData) insertTriceIDs(w io.Writer, liPath string, in []byte, a *ant.Ad
 						if Verbose {
 							fmt.Fprintln(w, "ids=", ids, "so take an ID from there.")
 						}
-						idn = 0 // Discard value found in src. (case line duplication)
+						if Verbose {
+							fmt.Fprintln(w, "Discard value found in src. (case line duplication):", idn)
+						}
+						idn = 0
 						goto idsLoopEntry
 					}
 				}
@@ -298,6 +330,102 @@ func (p *idData) insertTriceIDs(w io.Writer, liPath string, in []byte, a *ant.Ad
 	}
 	out = []byte(outs)
 	return
+}
+
+// evaluateTriceParameterCount analyzes rest, if it has the correct parameter count according to t.
+// rest starts immediately after the trice format string and can be very long (the remaining file contet),
+// but we check only until the trice end and return nil if the evaluation is ok.
+func evaluateTriceParameterCount(t TriceFmt, line int, rest string) (err error) {
+	fsc := formatSpecifierCount(t.Strg)
+	// At this stage we know the end of the format string (start of rest)
+	// and also the count of the format specifier in it.
+	// What we do not know, is, where the right closing bracket is. So we can expect fsc colons now:
+	//     trice( "%02x, %02x", a, (b+c) );
+	// But there are special cases, like:
+	//     triceS( "%80s", ",,," );
+	//     triceB( "dbg: %02X\n", buf, sizeof(buf));
+	//     trice16F( "rpc:AFunctionName", buf, sizeof(buf));
+	//     trice( "%d", SUM(3,4));
+	cnt, err := countColonsUntilClosingBracket(rest)
+	if err != nil {
+		return err
+	}
+	lastChar := t.Type[len(t.Type)-1:]
+	switch lastChar {
+	case "S": // expect one colon followed by a string and a closing bracket
+		if fsc != 1 {
+			return fmt.Errorf("line %d %v should have exactly one format specifier and not %d", line, t, fsc)
+		}
+		if cnt != 1 {
+			return fmt.Errorf("line %d %v should have exactly one parameter and not %d", line, t, cnt)
+		}
+	case "N":
+		if fsc != 1 {
+			return fmt.Errorf("line %d %v should have exactly one format specifier and not %d", line, t, fsc)
+		}
+		if cnt != 2 {
+			return fmt.Errorf("line %d %v should have exactly two parameters and not %d", line, t, cnt)
+		}
+	case "B":
+		if fsc != 1 {
+			return fmt.Errorf("line %d %v should have exactly one format specifier and not %d", line, t, fsc)
+		}
+		if cnt != 2 {
+			return fmt.Errorf("line %d %v should have exactly two parameters and not %d", line, t, cnt)
+		}
+	case "F":
+		if fsc != 0 {
+			return fmt.Errorf("line %d %v should have no format specifier and not %d", line, t, fsc)
+		}
+		if cnt != 2 {
+			return fmt.Errorf("line %d %v should have exactly two parameters and not %d", line, t, cnt)
+		}
+	default:
+		if strings.Contains(t.Type, "Assert") { // matches triceAssertTrue and triceAssertFalse
+			if fsc != 0 || cnt != 1 {
+				return fmt.Errorf("line %d %v should have no format specifiers and eactly one parameter, the boolean value (fsc=%d, cnt=%d)", line, t, fsc, cnt)
+			}
+			return
+		}
+		if fsc != cnt {
+			return fmt.Errorf("line %d format specifier count %d != parameter count %d for %v", line, fsc, cnt, t)
+		}
+	}
+	return
+}
+
+// countColonsUntilClosingBracket parses s until the first closing bracket
+// and determines the number of colons.
+// If there is in rest a starting string, all until the string end is ignored.
+// If there is in rest a opening bracket, all until the next matching closing bracket is ignored.
+func countColonsUntilClosingBracket(rest string) (count int, e error) {
+	s := rest
+restart:
+	for i, c := range s {
+		switch c {
+		case '"':
+			loc := matchStringLiteral(s[i:])
+			if loc == nil {
+				return count, errors.New("invalid:" + rest)
+			}
+			s = s[i:]
+			s = s[loc[1]:]
+			goto restart
+		case '(':
+			s = s[i+1:] // cut off including '('
+			pos, e := matchBracketLiteral(s)
+			if e != nil {
+				return count, fmt.Errorf("invalid:%s", rest)
+			}
+			s = s[pos+1:] // cut off including ')'
+			goto restart
+		case ',':
+			count++
+		case ')':
+			return count, nil
+		}
+	}
+	return count, errors.New("no matching closing bracket found in: " + rest)
 }
 
 // writeID inserts id into s according to loc information and returns the result together with the changed len.
@@ -381,16 +509,56 @@ var stringLiterals bufio.SplitFunc = func(data []byte, atEOF bool) (advance int,
 	return start, nil, nil
 }
 
-// matchFormatString returns a two-element slice of integers defining the location of the leftmost match in s of the matchFmtString regular expression.
+// matchStringLiteral returns a two-element slice of integers defining the location of the leftmost match in s of the matchFmtString regular expression.
 // The match itself is at s[loc[0]:loc[1]]. A return value of nil indicates no match.
 // If the format string contains `\"` elements, the found sub strings are concatenated to the returned result.
-func matchFormatString(input string) (loc []int) {
+func matchStringLiteral(input string) (loc []int) {
 	scanner := bufio.NewScanner(strings.NewReader(input))
 	scanner.Split(stringLiterals)
 	if scanner.Scan() {
 		loc = append(loc, strings.Index(input, scanner.Text()))
 		loc = append(loc, loc[0]+len(scanner.Text()))
 	}
+	return
+}
+
+// matchBracketLiteral assumes, the input starts with the very first character after an opening '('
+// and returns the index of the closing ')'. Examples:
+//
+//	input,          index,  e
+//	")"		          0   nil
+//	"), )"		      0   nil
+//	"M(a,")"), a)"	 11   nil
+//	"."               0   err
+func matchBracketLiteral(input string) (index int, e error) {
+	s := input
+restart:
+	for i, c := range s {
+		switch c {
+		case ')':
+			index += i
+			return
+		case '(': // enter recursion
+			s = s[i+1:]    // cut off until behind '('
+			index += i + 1 // adjust
+			var idx int
+			idx, e = matchBracketLiteral(s)
+			if e != nil {
+				return
+			}
+			s = s[idx+1:]    // cut off until behind ')'
+			index += idx + 1 // adjust
+			goto restart
+		case '"':
+			s = s[i:]
+			index += i
+			loc := matchStringLiteral(s)
+			s = s[loc[1]:]
+			index += loc[1]
+			goto restart
+		}
+	}
+	e = errors.New("no closing bracket in:" + input)
 	return
 }
 
