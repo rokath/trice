f8fcc07cdac4d9046ad277eb5cfaa6bcd3fa4fe6
Thomas Höhenleitner <th@seerose.net>
2024-09-03 13:43:56 +0300

    c++ frame added

--- src/cobs.h
+++ src/cobs.h
@@ -4,6 +4,10 @@
 #ifndef COBS_H_
 #define COBS_H_
 
+#ifdef __cplusplus
+	extern "C" {
+#endif
+
 #include <stddef.h> //lint !e537 !e451  Warning 537: Repeated include file,  Warning 451: Header file repeatedly included but does not have a standard
 #include <stdint.h> //lint !e537 !e451  Warning 537: Repeated include file,  Warning 451: Header file repeatedly included but does not have a standard
 
@@ -12,5 +16,9 @@ size_t COBSEncode(void* __restrict out, const void* __restrict in, size_t length
 
 //! If your compiler uses a pre-C99 C dialect and does not know The "__restrict" keyword, you can define it in the project settings.
 size_t COBSDecode(void* __restrict out, const void* __restrict in, size_t length);
+
+#ifdef __cplusplus
+	}
+#endif
 
 #endif
