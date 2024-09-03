95ea617ff7aa462571abe1031fed0977bf02310f
Thomas Höhenleitner <th@seerose.net>
2024-09-03 15:29:11 +0300

    comments updated

--- src/triceDefaultConfig.h
+++ src/triceDefaultConfig.h
@@ -10,19 +10,19 @@
 #endif
 
 #ifndef TRICE_UARTA_MIN_ID
-	#define TRICE_UARTA_MIN_ID 0
+	#define TRICE_UARTA_MIN_ID 0 //!< TRICE_UARTA_MIN_ID, if > 0, is the smalles ID routed to UARTA.
 #endif
 
 #ifndef TRICE_UARTA_MAX_ID
-	#define TRICE_UARTA_MAX_ID 0
+	#define TRICE_UARTA_MAX_ID 0 //!< TRICE_UARTA_MAX_ID, if > 0, is the largest ID routed to UARTA.
 #endif
 
 #ifndef TRICE_UARTB_MIN_ID
-	#define TRICE_UARTB_MIN_ID 0
+	#define TRICE_UARTB_MIN_ID 0 //!< TRICE_UARTB_MIN_ID, if > 0, is the smalles ID routed to UARTB.
 #endif
 
 #ifndef TRICE_UARTB_MAX_ID
-	#define TRICE_UARTB_MAX_ID 0
+	#define TRICE_UARTB_MAX_ID 0 //!< TRICE_UARTB_MAX_ID, if > 0, is the largest ID routed to UARTA.
 #endif
 
 #ifndef TRICE_DEFERRED_UARTA
@@ -88,14 +88,14 @@
 #endif
 
 #ifndef TriceStamp16
-	//! TriceStamp16 returns a 16-bit value to stamp `Id` TRICE macros. Usually it is a timestamp, but could also be a destination address or a counter for example.
-	//! The user has to provide this function. Defining a macro here, instead if providing `int16_t TriceStamp16( void );` has significant speed impact.
+	//! TriceStamp16 returns a 16-bit value to stamp `Id` TRICE and `Trice` macros. Usually it is a timestamp, but could also be a destination address or a counter for example.
+	//! The user has to provide this value. Defining a macro here, instead if providing a fuction has significant speed impact.
 	#define TriceStamp16 0x1616
 #endif
 
 #ifndef TriceStamp32
-	//! TriceStamp32 returns a 32-bit value to stamp `ID` TRICE macros. Usually it is a timestamp, but could also be a destination address or a counter for example.
-	//! The user has to provide this function. Defining a macro here, instead if providing `int32_t TriceStamp32( void );` has significant speed impact.
+	//! TriceStamp32 returns a 32-bit value to stamp `ID` TRICE and `TRice` macros. Usually it is a timestamp, but could also be a destination address or a counter for example.
+	//! The user has to provide this function. Defining a macro here, instead a function if providing has significant speed impact.
 	#define TriceStamp32 0x32323232
 #endif
 
@@ -119,7 +119,7 @@
 	//! - TRICE_DOUBLE_BUFFER: TRICE macros write direct into a double buffer without any additional management action.
 	//!   This is the fastest execution option for TRICE macros but needs more RAM. Used for deferred output and optional additional direct output.
 	//! - TRICE_RING_BUFFER: TRICE macros write direct into a ring buffer without any additional management action.
-	//!   This is a fast but not the fastest execution option for TRICE macros but needs less RAM. Used for deferred output and optional additional direct output.
+	//!   This is a fast but not the fastest execution option for TRICE macros and needs less RAM. Used for deferred output and optional additional direct output.
 	#define TRICE_BUFFER TRICE_DOUBLE_BUFFER
 #endif
 
@@ -128,7 +128,7 @@
 	//! - TRICE_SINGLE_PACK_MODE packs each Trice message separately and adds a 0-delimiter byte. This reduces the transmit byte count. In case of a lost package only one Trice can get lost.
 	//! - TRICE_MULTI_PACK_MODE packs several trice messages before adding a 0-delimiter byte. This reduces the transmit byte count. In case of a lost package several Trices can get lost.
 	//! - When using encryption, the TRICE_MULTI_PACK_MODE can significantly reduce the transmit byte count, because in TRICE_SINGLE_PACK_MODE each Trice message gets extended
-	//! with 1 to 7 padding bytes before encryption.
+	//! with 1 to 7 padding bytes before encryption. TRICE_MULTI_PACK_MODE is implemented for TRICE_DOUBLE_BUFFER but not yet for TRICE_RING_BUFFER and not possible for TRICE_STACK_BUFFER and TRICE_STATIC_BUFFER.
 	#define TRICE_DEFERRED_TRANSFER_MODE TRICE_SINGLE_PACK_MODE
 #endif
 
@@ -158,14 +158,14 @@
 
 #ifndef TRICE_DIRECT_OUTPUT_IS_WITH_ROUTING
 	//! TRICE_DIRECT_OUTPUT_IS_WITH_ROUTING == 1 makes only sense, when TRICE_DIRECT_OUTPUT is 1.
-	//! Enable this only, if you want only a specific ID ranges for direct Trice output.
+	//! Enable this only, if you want only a specific ID range for direct Trice output.
 	//! If you use different direct output channels, you can change this only for all together.
 	#define TRICE_DIRECT_OUTPUT_IS_WITH_ROUTING 0 // (experimental)
 #endif
 
 #ifndef TRICE_DEFERRED_OUTPUT_IS_WITH_ROUTING
 	//! TRICE_DEFERRED_OUTPUT_IS_WITH_ROUTING == 1 makes only sense, when TRICE_DEFERRED_OUTPUT is 1.
-	//! Enable this only, if you want only a specific ID ranges for direct Trice output.
+	//! Enable this only, if you want only a specific ID range for direct Trice output.
 	//! If you use different direct output channels, you can change this only for all together.
 	#define TRICE_DEFERRED_OUTPUT_IS_WITH_ROUTING 0 // (experimental)
 #endif
@@ -183,14 +183,16 @@
 #endif
 
 #ifndef TRICE_SINGLE_MAX_SIZE
-	//! TRICE_SINGLE_MAX_SIZE is used to truncate long dynamically generated strings, to detect the need of a ring buffer wrap or to protect against overflow.
+	//! TRICE_SINGLE_MAX_SIZE is used to truncate long runtime generated strings, to detect the need of a ring buffer wrap or to protect against overflow.
 	//! - Be careful with this value: When using 12 64-bit values with a 32-bit stamp the trice size is 2(id) + 4(stamp) + 2(count) + 12*8(values) = 104 bytes.
-	//! - In direct mode, and also when you enabled TRICE_DEFERRED_SEGGER_RTT_8BIT_WRITE, this plus TRICE_DATA_OFFSET is the max allocation size on the target
+	//! - In direct mode, and also when you enabled TRICE_DEFERRED_SEGGER_RTT_8BIT_WRITE, this plus TRICE_DATA_OFFSET plus 4 is the max allocation size on the target
 	//!   stack with TRICE_BUFFER == TRICE_STACK_BUFFER.
-	//! - When short of RAM and, for example, max 2 32-bit values with a 32-bit stamp are used, the max trice size is 2 + 4 + 2 + 2*4 = 16 bytes.
-	//! - You should then also disable all then forbidden trices to avoid mistakes. Example: `#define ENABLE_TRice32fn_3 0` and so on at the end of this file.
-	//! - When NOT using dynamic string (or buffer) transfer, bigger values than 104 make no sense here and just spoiling RAM.
-	//! - When USING dynamic string (or buffer) transfer, this value limits the max length of a trice. 2^15-1=32787 is the max possible value if you have enough RAM.
+	//! - When short of memory and, for example, max 2 32-bit values with a 32-bit stamp are used, the max trice size is 2 + 4 + 2 + 2*4 = 16 bytes.
+	//! - When not using target timestamps and not more than one 32-bit or two 16-bit or four 8-bit values or using target timestamp without carried values, the TRICE_SINGLE_MAX_SIZE should be 8 bytes only.
+	//! - With a value < 104 automatically all then forbidden trices are disabled to avoid mistakes. This also reduces the needed flash size and compile time depending on the used compiler.
+	//! - Example: TRrice( "%d %d %d", 1, 2, 3)` would cause a compiler error with `#define TRICE_SINGLE_MAX_SIZE 16` then.
+	//! - When NOT using dynamic string or buffer transfer, bigger values than 104 make no sense here and just spoiling RAM.
+	//! - When USING dynamic string (or buffer) transfer, this value limits the max length of a trice. 2^15-4=32764 is the max possible value if you have enough RAM.
 	#define TRICE_SINGLE_MAX_SIZE 104 // must be a multiple of 4
 #endif
 
@@ -205,14 +207,15 @@
 
 #ifndef TRICE_MCU_IS_BIG_ENDIAN
 	//! TRICE_MCU_IS_BIG_ENDIAN needs to be 1 for TRICE64 macros on big endian MCUs for correct 64-bit values and 32-bit timestamp encoding.
-	#define TRICE_MCU_IS_BIG_ENDIAN 0
+	#define TRICE_MCU_IS_BIG_ENDIAN 0 // todo: Set this value automatically thru the used compiler.
 #endif
 
 #ifndef TRICE_TRANSFER_ORDER_IS_NOT_MCU_ENDIAN
 	//! TRICE_TRANSFER_ORDER_IS_NOT_MCU_ENDIAN can be defined to 1 on little endian MCUs if the trice data are needed in network order,
 	//! or on big endian MCUs if the trice data are needed in little endian order. You should avoid setting this to 1 because
-	//! it increases the trice storage time and the needed code amount.
-	//! This is not completely implemented and needs automatic tests as well.
+	//! it increases the trice storage time and the needed code amount. The default transfer order is little endian as most targets are 
+	//! little endian machines. If you change the transfer order to big endian here, you need to apply `-triceEndianness bigEndian` CLI switch. 
+	//! when using the `trice log` command. This may be is not completely implemented and needs automatic tests as well.
 	//! The main to implement:
 	//! - implement compiler agnostic access macros (byte swapping)
 	//! - The TriceIDAndBuffer function and it relatives need to use access macros.
@@ -243,17 +246,19 @@
 #endif
 
 #ifndef XTEA_DECRYPT
-	//! XTEA_DECRYPT, when defined, enables device local decryption. Usable for checks or if you use a trice capable node to read XTEA encrypted messages.
+	//! XTEA_DECRYPT, when defined, enables device local decryption. Usable for checks or if you use a trice reception capable node to read XTEA encrypted messages.
+	//! One possible application is, receiving trices, decoding them, finding an ID match in a (from triceF generated) function pointer list and executing the dedicated function.
+	//! This allows kind of RPC (without immediate return) also with encryption. 
 	#define XTEA_DECRYPT 0
 #endif
 
 #ifndef TRICE_DIRECT_XTEA_ENCRYPT
-	//! TRICE_DIRECT_XTEA_ENCRYPT enables encryption for direct output. (experimental)
+	//! TRICE_DIRECT_XTEA_ENCRYPT enables encryption for direct output. Encrypting direct output costs significant computing time in the time critical path!  
 	#define TRICE_DIRECT_XTEA_ENCRYPT 0
 #endif
 
 #ifndef TRICE_DEFERRED_XTEA_ENCRYPT
-	//! TRICE_DEFERRED_XTEA_ENCRYPT enables encryption for deferred output. (experimental)
+	//! TRICE_DEFERRED_XTEA_ENCRYPT enables encryption for deferred output.
 	#define TRICE_DEFERRED_XTEA_ENCRYPT 0
 #endif
 
@@ -274,41 +279,41 @@
 	//! TRICE_DIRECT_SEGGER_RTT_32BIT_WRITE == 1 speeds up RTT transfer by using function SEGGER_Write_RTT0_NoCheck32.
 	//! - This setting results in unframed RTT trice packages and requires the `-packageFraming none` switch for the appropriate trice tool instance.
 	//!   This squeezes the whole TRICE macro into about 100 processor clocks leaving the data already inside the SEGGER _acUpBuffer.
-	//! - If you do not wish RTT, or with RTT with framing, simply set this value to 0.
+	//! - If you do not wish RTT, or wish RTT with framing, simply set this value to 0.
 	//! - The trice tool CLI switch -d16 is needed too, because for alignment reasons the 16bit ID field is doubled for 16bit timestamp trice messages.
 	#define TRICE_DIRECT_SEGGER_RTT_32BIT_WRITE 0
 #endif
 
 #ifndef TRICE_DEFERRED_SEGGER_RTT_8BIT_WRITE
 	//! TRICE_DEFERRED_SEGGER_RTT_8BIT_WRITE == 1 enables channel number 0 for SeggerRTT usage. Only channel 0 works right now for some reason.
-	//! Than the RTT trice packages are can be framed according to the set TRICE_DIRECT_OUT_FRAMING.
-	//! Not useable with TRICE_DIRECT_SEGGER_RTT_32BIT_WRITE or TRICE_DIRECT_SEGGER_RTT_8BIT_WRITE
-	//! Switch this on, if you wish deferred Segger RTT output.
+	//! Than the RTT trice packages can be framed according to the set TRICE_DIRECT_OUT_FRAMING.
+	//! Not useable together with TRICE_DIRECT_SEGGER_RTT_32BIT_WRITE or TRICE_DIRECT_SEGGER_RTT_8BIT_WRITE
+	//! Switch this on, if you wish deferred Segger RTT output. Usually RTT is used for direct mode only.
 	#define TRICE_DEFERRED_SEGGER_RTT_8BIT_WRITE 0
 #endif
 
 #ifndef TRICE_DIRECT_AUXILIARY8
-	//! TRICE_DIRECT_AUXILIARY8 enables a user defined direct trice 8bit char write. (experimental)
+	//! TRICE_DIRECT_AUXILIARY8 enables a user defined direct trice 8bit char write.
 	#define TRICE_DIRECT_AUXILIARY8 0
 #endif
 
 #ifndef TRICE_DIRECT_AUXILIARY32
-	//! TRICE_DIRECT_AUXILIARY32 enables a user defined direct trice 32bit word write. (experimental)
+	//! TRICE_DIRECT_AUXILIARY32 enables a user defined direct trice 32bit word write.
 	#define TRICE_DIRECT_AUXILIARY32 0
 #endif
 
 #ifndef TRICE_DEFERRED_AUXILIARY8
-	//! TRICE_DEFERRED_AUXILIARY8 enables a user defined deferred trice 8bit char write. (experimental)
+	//! TRICE_DEFERRED_AUXILIARY8 enables a user defined deferred trice 8bit char write.
 	#define TRICE_DEFERRED_AUXILIARY8 0
 #endif
 
 #ifndef TRICE_DEFERRED_AUXILIARY32
-	//! TRICE_DEFERRED_AUXILIARY32 enables a user defined deferred trice 32bit word write. (experimental)
+	//! TRICE_DEFERRED_AUXILIARY32 enables a user defined deferred trice 32bit word write.
 	#define TRICE_DEFERRED_AUXILIARY32 0
 #endif
 
 #ifndef TRICE_CGO
-	//! CGO interface (for testing the target code with Go only, do not enable)
+	//! CGO interface for testing the target code with Go only, do not enable normally. Usage examples can be found in the trice/test folder.
 	#define TRICE_CGO 0
 #endif
 
@@ -322,52 +327,52 @@
 // Compiler Adaption:
 
 #ifndef USE_SEGGER_RTT_LOCK_UNLOCK_MACROS
-	//! USE_SEGGER_RTT_LOCK_UNLOCK_MACROS == 1 includes SEGGER_RTT header files even SEGGER_RTT is not used.
+	//! USE_SEGGER_RTT_LOCK_UNLOCK_MACROS == 1 includes SEGGER_RTT header files even SEGGER_RTT is not used. THis allows SEGGER code for critical sections.
 	#define USE_SEGGER_RTT_LOCK_UNLOCK_MACROS 0
 #endif
 
 #ifndef TRICE_ENTER_CRITICAL_SECTION
 	//! TRICE_ENTER_CRITICAL_SECTION saves interrupt state and disables Interrupts.
-	//! If trices are used only outside critical sections or interrupts,
-	//! you can leave this macro empty for more speed. Use only '{' in that case.
-	//! #define TRICE_ENTER_CRITICAL_SECTION { SEGGER_RTT_LOCK() { - does the job for many compilers.
-	//! #define TRICE_ENTER_CRITICAL_SECTION {
-	//! #define TRICE_ENTER_CRITICAL_SECTION { uint32_t old_mask = cm_mask_interrupts(1); { // copied from test/OpenCM3_STM32F411_Nucleo/triceConfig.h
-	//! #define TRICE_ENTER_CRITICAL_SECTION { uint32_t primaskstate = __get_PRIMASK(); __disable_irq(); {
+	//! If trices are used only outside critical sections or interrupts, you can leave this macro empty for more speed. Use only '{' in that case.
+	//! Examples:
+	//! - #define TRICE_ENTER_CRITICAL_SECTION { SEGGER_RTT_LOCK() { - does the job for many compilers.
+	//! - #define TRICE_ENTER_CRITICAL_SECTION {
+	//! - #define TRICE_ENTER_CRITICAL_SECTION { uint32_t old_mask = cm_mask_interrupts(1); { // copied from test/OpenCM3_STM32F411_Nucleo/triceConfig.h
+	//! - #define TRICE_ENTER_CRITICAL_SECTION { uint32_t primaskstate = __get_PRIMASK(); __disable_irq(); {
 	#define TRICE_ENTER_CRITICAL_SECTION {
 #endif
 
 #ifndef TRICE_LEAVE_CRITICAL_SECTION
 	//! TRICE_LEAVE_CRITICAL_SECTION restores interrupt state.
-	//! If trices are used only outside critical sections or interrupts,
-	//! you can leave this macro empty for more speed. Use only '}' in that case.
-	//! #define TRICE_LEAVE_CRITICAL_SECTION } SEGGER_RTT_UNLOCK() } - does the job for many compilers.
-	//! #define TRICE_LEAVE_CRITICAL_SECTION }
-	//! #define TRICE_LEAVE_CRITICAL_SECTION } cm_mask_interrupts(old_mask); } // copied from test/OpenCM3_STM32F411_Nucleo/triceConfig.h
-	//! #define TRICE_LEAVE_CRITICAL_SECTION } __set_PRIMASK(primaskstate); }
+	//! If trices are used only outside critical sections or interrupts, you can leave this macro empty for more speed. Use only '}' in that case.
+	//! Examples:
+	//! - #define TRICE_LEAVE_CRITICAL_SECTION } SEGGER_RTT_UNLOCK() } - does the job for many compilers.
+	//! - #define TRICE_LEAVE_CRITICAL_SECTION }
+	//! - #define TRICE_LEAVE_CRITICAL_SECTION } cm_mask_interrupts(old_mask); } // copied from test/OpenCM3_STM32F411_Nucleo/triceConfig.h
+	//! - #define TRICE_LEAVE_CRITICAL_SECTION } __set_PRIMASK(primaskstate); }
 	#define TRICE_LEAVE_CRITICAL_SECTION }
 #endif
 
 #ifndef TRICE_INLINE
-	#define TRICE_INLINE static inline //! used for trice code
+	#define TRICE_INLINE static inline //! TRICE_INLINE is used for inlining trice code.
 #endif
 
 // code space optimization
 
 #ifndef TRICE_8_BIT_SUPPORT
-	#define TRICE_8_BIT_SUPPORT 1 //!< Enables/Disables all 8-bit Trice macros.
+	#define TRICE_8_BIT_SUPPORT 1 //!< TRICE_8_BIT_SUPPORT enables/disables all 8-bit Trice macros.
 #endif
 
 #ifndef TRICE_16_BIT_SUPPORT
-	#define TRICE_16_BIT_SUPPORT 1 //!< Enables/Disables all 16-bit Trice macros.
+	#define TRICE_16_BIT_SUPPORT 1 //!< TRICE_16_BIT_SUPPORT enables/disables all 16-bit Trice macros.
 #endif
 
 #ifndef TRICE_32_BIT_SUPPORT
-	#define TRICE_32_BIT_SUPPORT 1 //!< Enables/Disables all 32-bit Trice macros.
+	#define TRICE_32_BIT_SUPPORT 1 //!< TRICE_32_BIT_SUPPORT enables/disables all 32-bit Trice macros.
 #endif
 
 #ifndef TRICE_64_BIT_SUPPORT
-	#define TRICE_64_BIT_SUPPORT 1 //!< Enables/Disables all 16-bit Trice macros.
+	#define TRICE_64_BIT_SUPPORT 1 //!< TRICE_64_BIT_SUPPORT enables/disables all 16-bit Trice macros.
 #endif
 
 // According to TRICE_SINGLE_MAX_SIZE the Trice functions are enabled/disabled automatically.
