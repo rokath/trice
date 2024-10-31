8330b41fd436873b4b294b9d8fe40bd0b9ed9a48
rokath <th@seerose.net>
2024-10-31 21:25:11 +0100

    MacOS adapted

--- examples/G0B1_inst/build.sh
+++ examples/G0B1_inst/build.sh
@@ -5,6 +5,6 @@ trice add -src ../../_test/testdata -src ../exampleData # Make sure, these go un
 # The folders  ../../_test/testdata  and ../exampleData are used by several projects and therefore not cleaned.
 trice clean -src ./Core -cache # Run this first to trigger the used editor to show the Trice IDs cleaned state.
 trice insert -src ../../_test/testdata -src ../exampleData -src ./Core  -IDMin 16200 -IDMax 16383 -IDMethod upward -cache # Add project data.
-make -j $(nproc --all) # Windows
-#make -j $(sysctl -n hw.ncpu) # MacOS
+#make -j $(nproc --all) # Windows
+make -j $(sysctl -n hw.ncpu) # MacOS
 trice clean -src ./Core -cache # Run this again to show the Trice IDs cleaned state.
