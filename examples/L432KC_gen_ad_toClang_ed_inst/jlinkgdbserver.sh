427afb3a5b5d646005522e0f1e38b787ba60529f
rokath <th@seerose.net>
2024-11-14 21:00:39 +0100

    openocd wip

---
+++ examples/L432KC_gen_ad_toClang_ed_inst/jlinkgdbserver.sh
@@ -0,0 +1,23 @@
+JLinkGDBServer \
+-device STM32L432KC \
+-if SWD \
+-speed 4000 \
+-notimeout \
+-noir \
+-nohalt \
+-noreset \
+-RTTTelnetPort 19021 \
+
+# -endian little \
+# -nolocalhostonly \
+# -port 3333 \
+# -telnetport 2333 \
+# -notimeout \
+# -select USB \
+# -strict \
+# -RTTTelnetPort 4444 \
+
+# -settingsfile Selects the J-Link Settings File.
+# -jlinkscriptfile “C:\My.JLinkScript” (7.13)
+# -x Executes a gdb file on first connection.
+# -xc Executes a gdb file on every connection.
