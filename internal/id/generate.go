fd6693351c0f06c7cc6a082002af809d9e5ec137
Thomas Höhenleitner <th@seerose.net>
2024-12-10 16:08:26 +0100

    C# generator base function ok

--- internal/id/generate.go
+++ internal/id/generate.go
@@ -220,126 +220,53 @@ func (ilu TriceIDLookUp) ToFileCSharp(fSys afero.Fs, fn string) (err error) {
 
 // toCSFmtList converts ilu into CS-source byte slice in human-readable form.
 func (ilu TriceIDLookUp) toCSFmtList(filename string) ([]byte, error) {
-	c := []byte(`//! \file ` + filename + ` 
+	text := []byte(`//! \file til.cs 
 
 // Trice generated code - do not edit!
 
-// There is still a need to exchange the format specifier from C to C#.
+// There is still a need to exchange the format specifier from C to C# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 // See https://stackoverflow.com/questions/33432341/how-to-use-c-language-format-specifiers-in-c-sharp
 // and https://www.codeproject.com/Articles/19274/A-printf-implementation-in-C for possible help.
 
 namespace TriceIDList;
 
-    public class TilItem
-    {
-        public TilItem(string strg, int bitWidth, int size)
-        {
-            Strg = strg;
-            BitWidth = bitWidth;
-            Size = size;
-        }
+	public class TilItem
+	{
+		public TilItem(int bitWidth, int paramCount, string strg)
+		{
+			BitWidth = bitWidth;
+			ParamCount = paramCount;
+			Strg = strg;
+		}
 
-        public string Strg { get; init; }
-        public int BitWidth { get; init; }
-        public int Size { get; init; }
-    }
+		public int BitWidth { get; init; }
+		public int ParamCount { get; init; }
+		public string Strg { get; init; }
+	}
 
-    public static class Til
-    {
-        public static readonly Dictionary<int, TilItem> TilList= new Dictionary<int, TilItem>
-        { // id, TilItem ( Strg, bitWidth, dataLength )
+	//! Til contains all trice format strings together with id and parameter information.
+	//!
+	//! The bitWidth value is not transmitted in the binary data stream and needed for its decoding.
+	//! The paramCount is de-facto not needed. It is derivable from the received data, see docs/TriceUserManual.md/#binary-encoding.
+	//! It is recommended to check if both values are matching. A negative paramCount indicates, that its value is unknown at compile time.
+	public static class Til
+	{
+		public static readonly Dictionary<int, TilItem> TilList= new Dictionary<int, TilItem>
+		{ /* triceType ( extended ) */ //   id,     TilItem( bitWidth, paramCount, Strg )
 `)
-	var paramCount int
-	var bitWidth int
-	var dataLength int
-	var add bool
 
-	for id, k := range ilu {
-		switch strings.ToUpper(k.Type) {
+	defaultBitWidth, err := strconv.Atoi(DefaultTriceBitWidth)
+	msg.FatalOnErr(err)
 
-		case "TRICE0", "TRICE32", "TRICE32_1", "TRICE32_2", "TRICE32_3", "TRICE32_4", "TRICE32_5", "TRICE32_6", "TRICE32_7", "TRICE32_8", "TRICE32_9", "TRICE32_10", "TRICE32_11", "TRICE32_12":
-			bitWidth = 32
-			paramCount = formatSpecifierCount(k.Strg)
-			dataLength = paramCount * 4 // use for checks
-			add = true
-		case "TRICE16", "TRICE16_1", "TRICE16_2", "TRICE16_3", "TRICE16_4", "TRICE16_5", "TRICE16_6", "TRICE16_7", "TRICE16_8", "TRICE16_9", "TRICE16_10", "TRICE16_11", "TRICE16_12":
-			bitWidth = 16
-			paramCount = formatSpecifierCount(k.Strg)
-			dataLength = paramCount * 2 // use for checks
-			add = true
-		case "TRICE8", "TRICE8_1", "TRICE8_2", "TRICE8_3", "TRICE8_4", "TRICE8_5", "TRICE8_6", "TRICE8_7", "TRICE8_8", "TRICE8_9", "TRICE8_10", "TRICE8_11", "TRICE8_12":
-			bitWidth = 8
-			paramCount = formatSpecifierCount(k.Strg)
-			dataLength = paramCount * 1 // use for checks
-			add = true
-		case "TRICE64", "TRICE64_1", "TRICE64_2", "TRICE64_3", "TRICE64_4", "TRICE64_5", "TRICE64_6", "TRICE64_7", "TRICE64_8", "TRICE64_9", "TRICE64_10", "TRICE64_11", "TRICE64_12":
-			bitWidth = 64
-			paramCount = formatSpecifierCount(k.Strg)
-			dataLength = paramCount * 8 // use for checks
-			add = true
-		case "TRICE_S":
-			bitWidth = 8
-			paramCount = 1
-			dataLength = -1 // needs to be derived
-			add = true
-		case "TRICE_N":
-			bitWidth = 8
-			paramCount = 1
-			dataLength = -2 // needs to be derived, add terminating 0
-			add = true
-		case "TRICE_B", "TRICE8_B":
-			bitWidth = 8
-			paramCount = 1
-			dataLength = -8 // needs to be derived
-			add = false     // provide generated format string
-		case "TRICE16_B":
-			bitWidth = 16
-			paramCount = 1
-			dataLength = -16 // needs to be derived
-			add = false      // provide generated format string
-		case "TRICE32_B":
-			bitWidth = 32
-			paramCount = 1
-			dataLength = -32 // needs to be derived
-			add = false      // provide generated format string
-		case "TRICE64_B":
-			bitWidth = 64
-			paramCount = 1
-			dataLength = -64 // needs to be derived
-			add = false      // provide generated format string
-		case "TRICE8_F", "TRICE_F": // example: TRICE_F( Id( 2844), "info:FunctionNameW",  b8,  sizeof(b8) /sizeof(int8_t) );
-			bitWidth = 8
-			paramCount = 1
-			dataLength = -9 // needs to be derived
-			add = false     // provide generated format string
-		case "TRICE16_F":
-			bitWidth = 16
-			paramCount = 1
-			dataLength = -17 // needs to be derived
-			add = false      // provide generated format string
-		case "TRICE32_F":
-			bitWidth = 32
-			paramCount = 1
-			dataLength = -33 // needs to be derived
-			add = false      // provide generated format string
-		case "TRICE64_F":
-			bitWidth = 64
-			paramCount = 1
-			dataLength = -65 // needs to be derived
-			add = false      // provide generated format string
-		default:
-
-		}
-		if add {
-			c = append(c, []byte(fmt.Sprintf(`        { %5d, new TilItem( "%s", %d, %d ) },`+"\n", id, k.Strg, bitWidth, dataLength))...)
-			add = false
-		}
+	for id, t := range ilu {
+		extType, bitWidth, paramCount := computeValues(t, defaultBitWidth)
+		text = append(text, []byte(fmt.Sprintf(`		/* %10s ( %10s )*/ { %5d, new TilItem( %2d, %2d, "%s" ) },`+"\n", t.Type, extType, id, bitWidth, paramCount, t.Strg))...)
 	}
 
 	tail := []byte(`    };
 }
 
 `)
-	c = append(c, tail...)
-	return c, nil
+	text = append(text, tail...)
+	return text, nil
 }
