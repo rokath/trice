f2dbf365af10617086bec5ff058709d39960667d
rokath <th@seerose.net>
2025-01-13 10:37:22 +0000

    RTT script for Linux ok

--- examples/G0B1_inst/RTTLog.sh
+++ examples/G0B1_inst/RTTLog.sh
@@ -2,11 +2,8 @@
 
 # Needs "sudo apt install screen" done before.
 
-# https://superuser.com/questions/386059/how-can-i-start-multiple-screen-sessions-automatically
-
-# (not working yet)
-
-rm ./temp/trice.bin
+rm -f ./temp/trice.bin
 touch ./temp/trice.bin
-screen -dmS "RTT" JLinkRTTLogger "-Device STM32G0B1RE -If SWD -Speed 4000 -RTTChannel 0 ./temp/trice.bin"
+screen -d -m JLinkRTTLogger -Device STM32G0B1RE -If SWD -Speed 4000 -RTTChannel 0 ./temp/trice.bin
 trice log -p FILE -args ./temp/trice.bin -prefix off -hs off -d16 -ts ms  -i ../../demoTIL.json -li ../../demoLI.json -pf none
+screen -X quit
