a9159957fd518922a028f34efd21d04153ba42a3
rokath <th@seerose.net>
2025-01-16 07:19:34 +0100

    wip

--- examples/G0B1_inst/RTTLogTmux.sh
+++ examples/G0B1_inst/RTTLogTmux.sh
@@ -6,8 +6,8 @@
 mkdir -p ./temp
 rm -f ./temp/trice.bin
 touch ./temp/trice.bin
-tmux new -s "rttlog" -d "JLinkRTTLogger -Device STM32G0B1RE -If SWD -Speed 4000 -RTTChannel 0 ./temp/trice.bin"
+tmux new -s "tricerttlog" -d "JLinkRTTLogger -Device STM32G0B1RE -If SWD -Speed 4000 -RTTChannel 0 ./temp/trice.bin"
 trice log -p FILE -args ./temp/trice.bin -pf none -prefix off -hs off -d16 -ts16 "time:offs:%4d µs" -showID "deb:%5d" -i ../../demoTIL.json -li ../../demoLI.json
 #trice log -p FILE -args ./temp/trice.bin -pf none -prefix off -hs off -d16 -ts ms                                     -i ../../demoTIL.json -li ../../demoLI.json
 
-tmux kill-session -t "rttlog"
+tmux kill-session -t "tricerttlog"
