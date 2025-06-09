a248cd0d56a91e13d2c7c5e884095f86dca51f1e
srgg <srggal@gmail.com>
2025-06-08 19:38:45 -0600

    Updated G0B1_inst example to demonstrate the use of the Alias feature.

--- renewIDs_in_examples_and_refresh_test_folder.sh
+++ renewIDs_in_examples_and_refresh_test_folder.sh
@@ -5,7 +5,7 @@ LIP="-liPath relative"
 
 #rm -f          demoTIL.json     demoLI.json # forget history (users usually should not do that in their projects, delete to avoid potential ID conflict messages)
 #touch          demoTIL.json     demoLI.json # new life
-trice clean  -i demoTIL.json -li demoLI.json -src $TD -src ./examples # wipe out all IDs from the sources
+trice clean  -i demoTIL.json -li demoLI.json -src $TD -src ./examples -alias CUSTOM_PRINT -salias CUSTOM_ASSERT # wipe out all IDs from the sources
 #rm -f          demoTIL.json     demoLI.json # forget history (in case the sources contained IDs, these are now removed from there, but are kept in the *.json files, so delete them again.)
 #touch          demoTIL.json     demoLI.json # new life
 
@@ -15,7 +15,7 @@ trice clean  -i demoTIL.json -li demoLI.json -src $TD -src ./examples # wipe out
 trice insert -i demoTIL.json -li demoLI.json $LIP              -IDMax 16383 -IDMethod downward -src ./examples/exampleData/triceLogDiagData.c
 trice insert -i demoTIL.json -li demoLI.json $LIP              -IDMax 16383 -IDMethod downward -src ./examples/exampleData/triceExamples.c
 trice insert -i demoTIL.json -li demoLI.json $LIP -IDMin 13000 -IDMax 15999 -IDMethod upward   -src ./examples/F030_inst/Core
-trice insert -i demoTIL.json -li demoLI.json $LIP -IDMin 13000 -IDMax 15999 -IDMethod upward   -src ./examples/G0B1_inst/Core
+trice insert -i demoTIL.json -li demoLI.json $LIP -IDMin 13000 -IDMax 15999 -IDMethod upward   -src ./examples/G0B1_inst/Core -alias CUSTOM_PRINT -salias CUSTOM_ASSERT -exclude ./examples/G0B1_inst/Core/Inc/triceCustomAliases.h
 trice insert -i demoTIL.json -li demoLI.json $LIP -IDMin 13000 -IDMax 15999 -IDMethod upward   -src ./examples/L432_inst/Core
 trice insert -i demoTIL.json -li demoLI.json $LIP -IDMin 13000 -IDMax 15999 -IDMethod upward   -src $TD/triceCheck.c
 trice insert -i demoTIL.json -li demoLI.json $LIP -IDMin 13000 -IDMax 15999 -IDMethod upward   -src $TD/..
