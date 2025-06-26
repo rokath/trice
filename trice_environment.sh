56b936d77840b0445c94d4e6593111ffae1e1501
rokath <th@seerose.net>
2025-06-26 14:10:42 +0200

    trice settings more centralized to avoid issues

--- trice_environment.sh
+++ trice_environment.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-# Trice command line common part
-TRICE_CMD_LINE+="-cache "                # Do not forget to create ~/.trice/cache folder, if the Trice cache should work or disable this line.
-TRICE_CMD_LINE+="-i ./demoTIL.json "     # Use a common til.json for all examples and tests.
-TRICE_CMD_LINE+="-li ./demoLI.json "     # Use a common  li.json for all examples and tests.
-TRICE_CMD_LINE+="-liPath relative "      # Prefix base filenames in li.json with relative path for new IDs.
-TRICE_CMD_LINE+="-src ./_test "
-TRICE_CMD_LINE+="-src ./examples/exampleData "
-TRICE_CMD_LINE+="-src ./examples/F030_inst/Core "
-TRICE_CMD_LINE+="-src ./examples/G0B1_inst/Core "
-TRICE_CMD_LINE+="-src ./examples/L432_inst/Core "
+# trice command line common part
+TRICE_DEFAULTS+="-i ./demoTIL.json "     # Use a common til.json for all examples and tests.
+TRICE_DEFAULTS+="-li ./demoLI.json "     # Use a common  li.json for all examples and tests.
+TRICE_DEFAULTS+="-liPath relative "      # Prefix base filenames in li.json with relative path for new IDs.
+#TRICE_DEFAULTS+="-cache "                # Do not forget to create ~/.trice/cache folder, if the Trice cache should work or disable this line.
 
-# Aliases
-TRICE_CMD_LINE+="-alias CUSTOM_PRINT "
-TRICE_CMD_LINE+="-salias CUSTOM_ASSERT "
-TRICE_CMD_LINE+="-exclude ./examples/G0B1_inst/Core/Inc/triceCustomAliases.h "
+# trice user aliases
+TRICE_ALIASES+="-alias CUSTOM_PRINT "
+TRICE_ALIASES+="-salias CUSTOM_ASSERT "
+TRICE_ALIASES+="-exclude ./examples/G0B1_inst/Core/Inc/triceCustomAliases.h "
+TRICE_ALIASES+="-alias result "
+TRICE_ALIASES+="-alias tell "
+TRICE_ALIASES+="-exclude ./_test/be_dblB_de_tcobs_ua/xxx/aliasConfig.h "
+
+# trice user project files
+TRICE_PRJ_FILES+="-src ./_test "
+TRICE_PRJ_FILES+="-src ./examples/exampleData "
+TRICE_PRJ_FILES+="-src ./examples/F030_inst/Core "
+TRICE_PRJ_FILES+="-src ./examples/G0B1_inst/Core "
+TRICE_PRJ_FILES+="-src ./examples/L432_inst/Core "
--- trice_insertIDs_in_examples_and_test_folder.sh
+++ trice_insertIDs_in_examples_and_test_folder.sh
@@ -4,6 +4,6 @@ SCRIPT_DIRECTORY="$(dirname $(realpath "$0"))"
 cd $SCRIPT_DIRECTORY
 source ./trice_environment.sh
 
-trice insert $TRICE_CMD_LINE -IDMin 13000 -IDMax 16383
+trice insert $TRICE_DEFAULTS $TRICE_ALIASES $TRICE_PRJ_FILES -IDMin 13000 -IDMax 16383
 
 cd - > /dev/null
