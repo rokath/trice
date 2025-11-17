9b9a6f471c9fa51a2bfe83eb7a5f47683aac2670
Thomas Höhenleitner <th@seerose.net>
2025-11-17 15:54:47 +0100

    Test TRICE_OFF=1 translation with and without IDs

--- examples/G0B1_inst/build.sh
+++ examples/G0B1_inst/build.sh
@@ -13,7 +13,10 @@ done
 
 # Trice is called here and not within make, to guarantee, it is finished before any other job starts.
 ../../trice_cleanIDs_in_examples_and_test_folder.sh # Run this first to trigger the used editor to show the Trice IDs cleaned state.
+
+if [ "$triceOFF" != "1" ]; then
 ../../trice_insertIDs_in_examples_and_test_folder.sh # custom aliases should be excluded or without IDs, when translating with TRICE_OFF=1
+fi
 
 source ../../build_environment.sh
 make $MAKE_JOBS TRICE_FLAGS="$flags" gcc # We translate here with inserted IDs, even in the triceOff case and expect no warnings.
