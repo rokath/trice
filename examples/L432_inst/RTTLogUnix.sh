94377a3425e77c459fc82a6c0e498173d75aca41
Thomas Höhenleitner <th@seerose.net>
2025-12-02 14:58:15 +0100

    typo fixed

--- examples/L432_inst/RTTLogUnix.sh
+++ examples/L432_inst/RTTLogUnix.sh
@@ -5,6 +5,6 @@
 mkdir -p ./temp
 rm -f ./temp/trice.bin
 touch ./temp/trice.bin
-screen -d -m JLinkRTTLogger -Device STM32G0B1RE -If SWD -Speed 4000 -RTTChannel 0 ./temp/trice.bin
+screen -d -m JLinkRTTLogger -Device STM32L432KC -If SWD -Speed 4000 -RTTChannel 0 ./temp/trice.bin
 trice log -p FILE -args ./temp/trice.bin -pf none -prefix off -hs off -d16 -ts us -showID "deb:%5d" -i ../../demoTIL.json -li ../../demoLI.json
 screen -X quit
