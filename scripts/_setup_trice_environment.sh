0d7885cca12f0c9224356565971d21e700e19b55
Thomas Höhenleitner <th@seerose.net>
2026-07-27 22:34:06 +0200

    formatting

--- scripts/_setup_trice_environment.sh
+++ scripts/_setup_trice_environment.sh
@@ -7,8 +7,8 @@ TRICE_LI_JSON=${TRICE_LI_JSON:-./demoLI.json}
 TRICE_LI_PATH_GENERATION=${TRICE_LI_PATH_GENERATION:-relative}
 
 # trice command line common part
-TRICE_DEFAULTS+="-i ${TRICE_TIL_JSON} " # Use a common til.json for all examples and tests.
-TRICE_DEFAULTS+="-li ${TRICE_LI_JSON} " # Use a common  li.json for all examples and tests.
+TRICE_DEFAULTS+="-i ${TRICE_TIL_JSON} "                # Use a common til.json for all examples and tests.
+TRICE_DEFAULTS+="-li ${TRICE_LI_JSON} "                # Use a common  li.json for all examples and tests.
 TRICE_DEFAULTS+="-liPath ${TRICE_LI_PATH_GENERATION} " # Select the legacy File representation generated in li.json.
 #TRICE_DEFAULTS+="-cache "                # Do not forget to create ~/.trice/cache folder, if the Trice cache should work or disable this line.
 
